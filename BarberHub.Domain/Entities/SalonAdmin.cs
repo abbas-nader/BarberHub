d3c3238135dcd7a51f43a7ccd6bc1a0f4fab1350
Abbas <abbas.naderi.1388@gmail.com>
2026-07-28 18:14:42 +0330

    edit salonadmin entity

--- BarberHub.Domain/Entities/SalonAdmin.cs
+++ BarberHub.Domain/Entities/SalonAdmin.cs
@@ -5,7 +5,7 @@ public class SalonAdmin : BaseEntity
     public string FullName { get; set; } = null!;
     public string UserName { get; set; } = null!;
     public string PasswordHash { get; set; } = null!;
-    
+
     public long SalonId { get; set; }
     public Salon Salon { get; set; } = null!;
 }
