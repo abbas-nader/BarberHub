49a7e8a803500942e7d3a498f6c57344264a4c19
Abbas <abbas.naderi.1388@gmail.com>
2026-07-28 18:45:17 +0330

    add barber entity

---
+++ BarberHub.Domain/Entities/Barber.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberHub.Domain.Entities;
+
+public class Barber : BaseEntity
+{
+    public string FullName { get; private set; } = null!;
+    public string? Description { get;private set; }
+    public bool IsActive { get; private set; }
+
+    
+    public long SalonId { get;private set; }
+    public Salon Salon { get; private set; } = null!;
+}
