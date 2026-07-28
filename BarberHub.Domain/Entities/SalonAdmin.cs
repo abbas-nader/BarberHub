ee376b91523b70340b2b6a0006f9a0544d33a226
Abbas <abbas.naderi.1388@gmail.com>
2026-07-28 12:49:47 +0330

    add salonadmin entity

---
+++ BarberHub.Domain/Entities/SalonAdmin.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Domain.Entities;
+
+public class SalonAdmin : BaseEntity
+{
+    public string FullName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    
+    public long SalonId { get; set; }
+    public Salon Salon { get; set; } = null!;
+}
