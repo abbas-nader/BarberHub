9dd6f831c948e20958c50f231951f311d13d5666
Abbas <abbas.naderi.1388@gmail.com>
2026-07-29 20:00:01 +0330

    feat: Add Customer entity

---
+++ BarberHub.Domain/Entities/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Domain.Entities;
+
+public class Customer : BaseEntity
+{
+    public string FirstName { get; private set; } = null!;
+    public string LastName { get; private set; } = null!;
+    public string MobileNumber { get; private set; } = null!;
+}
