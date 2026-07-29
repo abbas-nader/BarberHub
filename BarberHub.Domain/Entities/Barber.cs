0881867876b4ab6f2330868c0ad3815f00862d6f
Abbas <abbas.naderi.1388@gmail.com>
2026-07-29 16:25:43 +0330

    add mobilenumber and pinhash propeeties to barber

--- BarberHub.Domain/Entities/Barber.cs
+++ BarberHub.Domain/Entities/Barber.cs
@@ -2,11 +2,11 @@
 
 public class Barber : BaseEntity
 {
-    public string FullName { get; private set; } = null!;
+    public string FullName { get;private set; } = null!;
+    public string MobileNumber { get; private set; } = null!;
+    public string PinHash { get;private set; } = null!;
     public string? Description { get;private set; }
     public bool IsActive { get; private set; }
-
     
     public long SalonId { get;private set; }
-    public Salon Salon { get; private set; } = null!;
 }
