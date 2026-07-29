8cea46c98fb449c34caf0b6374a664c067641278
Abbas <abbas.naderi.1388@gmail.com>
2026-07-29 23:55:56 +0330

    refactor: remove price and Barber navigation from Service and add SalonId

--- BarberHub.Domain/Entities/Service.cs
+++ BarberHub.Domain/Entities/Service.cs
@@ -7,8 +7,6 @@ public class Service : BaseEntity
     public string Name { get; private set; } = null!;
     public string? Description { get; private set; }
     public TimeSpan Duration { get; private set; }
-    public Money Price { get; private set; } = null!;
     
-    public long BarberId { get; private set; }
-    public Barber Barber { get; private set; } = null!;
+    public long SalonId { get; private set; }
 }
