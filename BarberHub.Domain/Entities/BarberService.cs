0b954a1ed6c3d14831c014ad8fac4aa11d62f573
Abbas <abbas.naderi.1388@gmail.com>
2026-07-29 23:55:06 +0330

    feat: add BarberService join entity

---
+++ BarberHub.Domain/Entities/BarberService.cs
@@ -0,0 +1,14 @@
+﻿using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Domain.Entities;
+
+public class BarberService : BaseEntity
+{
+    public long  BarberId { get; private set; }
+    public Barber Barber { get; private set; } = null!;
+    
+    public long ServiceId { get; private set; }
+    public Service Service { get; private set; } = null!;
+
+    public Money Money { get; private set; } = null!;
+}
