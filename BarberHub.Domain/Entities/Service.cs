3f877231159f04e451aa7efc299ee35a0d6639af
Abbas <abbas.naderi.1388@gmail.com>
2026-07-29 19:08:03 +0330

    feat: add service entity

---
+++ BarberHub.Domain/Entities/Service.cs
@@ -0,0 +1,14 @@
+﻿using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Domain.Entities;
+
+public class Service : BaseEntity
+{
+    public string Name { get; private set; } = null!;
+    public string? Description { get; private set; }
+    public TimeSpan Duration { get; private set; }
+    public Money Price { get; private set; } = null!;
+    
+    public long BarberId { get; private set; }
+    public Barber Barber { get; private set; } = null!;
+}
