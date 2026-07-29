a7cbdb664169e9486a26e959592dffa9cd0817b3
Abbas <abbas.naderi.1388@gmail.com>
2026-07-29 18:10:06 +0330

    add workschedule entity

---
+++ BarberHub.Domain/Entities/WorkSchedule.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Domain.Entities;
+
+public class WorkSchedule : BaseEntity
+{
+    public TimeOnly StartTime { get;private set; }
+    public TimeOnly EndTime { get;private set; }
+    public DayOfWeek DayOfWeek { get;private set; }
+    
+    public long BarberId { get;private set; }
+    public Barber Barber { get;private set; } = null!;
+}
