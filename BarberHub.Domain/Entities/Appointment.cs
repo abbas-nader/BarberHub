92f5edf38485d7371983ea70c75dba1a9e2dec86
Abbas <abbas.naderi.1388@gmail.com>
2026-07-30 14:56:42 +0330

    feat: define Appointment entity properties

--- BarberHub.Domain/Entities/Appointment.cs
+++ BarberHub.Domain/Entities/Appointment.cs
@@ -1,6 +1,19 @@
-﻿namespace BarberHub.Domain.Entities;
+﻿using BarberHub.Domain.Enums;
+using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Domain.Entities;
 
 public class Appointment : BaseEntity
 {
+    public DateOnly AppointmentDate {get; private set; }
+    public TimeOnly StartTime {get; private set;}
+    public TimeOnly EndTime {get; private set;}
+    public Status Status { get; private set; } 
+    public ServiceSnapshot ServiceSnapshot { get; private set; } = null!;
+    public Money DepositAmountSnapshot { get; private set; } = null!;
+    public DepositPaymentMethod DepositPaymentMethod { get; private set; }
+    public DepositStatus DepositStatus { get; private set; }
+    public DateTimeOffset? CancelledAt { get; private set; }
+    public NoShowDetectionType?  NoShowDetectionType { get; private set; }
     
 }
