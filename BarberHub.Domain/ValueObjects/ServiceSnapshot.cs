ede945ea26de1f35b2aff2602175ff4a2ae891c5
Abbas <abbas.naderi.1388@gmail.com>
2026-07-30 15:18:54 +0330

    feat: add ServiceSnapshot value object for appointments

---
+++ BarberHub.Domain/ValueObjects/ServiceSnapshot.cs
@@ -0,0 +1,20 @@
+﻿using BarberHub.Domain.Exceptions;
+
+namespace BarberHub.Domain.ValueObjects;
+
+public sealed record ServiceSnapshot
+{
+    public string ServiceName { get; }
+    public TimeSpan ServiceDuration { get; }
+    public Money ServicePrice { get; }
+
+    public ServiceSnapshot(string serviceName, TimeSpan serviceDuration, Money servicePrice)
+    {
+        if (serviceDuration.Ticks <= 0) throw new InvalidServiceDurationSnapshotException();
+        if (string.IsNullOrWhiteSpace(serviceName))
+            throw new InvalidServiceNameSnapshotException();
+        ServiceName = serviceName;
+        ServiceDuration = serviceDuration;
+        ServicePrice = servicePrice ?? throw new InvalidServicePriceSnapshotException();
+    }
+}
