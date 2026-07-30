91f26df5047949f9af86842b4a4ea768d7b285fa
Abbas <abbas.naderi.1388@gmail.com>
2026-07-30 15:11:26 +0330

    feat: add AppointmentStatus enum to Appointment

---
+++ BarberHub.Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Domain.Enums;
+
+public enum AppointmentStatus : byte
+{
+    Confirmed = 1,
+    Completed = 2,
+    CancelledByCustomer = 3,
+    CancelledBySalon = 4,
+    NoShow = 5
+}
