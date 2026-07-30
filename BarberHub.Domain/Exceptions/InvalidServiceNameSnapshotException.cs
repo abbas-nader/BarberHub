6bf61333e280c65ac65cb73740ebb5734ab5b7a6
Abbas <abbas.naderi.1388@gmail.com>
2026-07-30 15:27:38 +0330

    feat: add InvalidServiceNameSnapshotException

---
+++ BarberHub.Domain/Exceptions/InvalidServiceNameSnapshotException.cs
@@ -0,0 +1,6 @@
+﻿namespace BarberHub.Domain.Exceptions;
+
+public class InvalidServiceNameSnapshotException()
+    : Exception("Invalid service name snapshot. Service name cannot be null or empty.")
+{
+}
