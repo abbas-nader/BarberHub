9371ec83de1549d1a255405391e10422a167a7e3
Abbas <abbas.naderi.1388@gmail.com>
2026-07-30 15:27:24 +0330

    feat: add InvalidServiceDurationSnapshotException

---
+++ BarberHub.Domain/Exceptions/InvalidServiceDurationSnapshotException.cs
@@ -0,0 +1,6 @@
+﻿namespace BarberHub.Domain.Exceptions;
+
+public class InvalidServiceDurationSnapshotException()
+    : Exception("Invalid service duration snapshot. Duration must be greater than zero.")
+{
+}
