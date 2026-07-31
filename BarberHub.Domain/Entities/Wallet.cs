95f2fc36fff252c2f7231901c0c00493251187a7
Abbas <abbas.naderi.1388@gmail.com>
2026-07-31 19:36:55 +0330

    feat: add Wallet entity

---
+++ BarberHub.Domain/Entities/Wallet.cs
@@ -0,0 +1,14 @@
+﻿using BarberHub.Domain.Enums;
+using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Domain.Entities;
+
+public class Wallet : BaseEntity
+{
+    public Money Amount { get; private set; } = null!;
+    public TransactionType TransactionType { get; private set; }
+    public WalletTransactionReason WalletTransactionReason { get; private set; }
+    public Money BalanceAfterTransaction { get; private set; } = null!;
+    
+    
+}
