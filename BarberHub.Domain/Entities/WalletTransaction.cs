750efec187fe00f7bb28d1b47be9111bc0c662e8
Abbas <abbas.naderi.1388@gmail.com>
2026-07-31 21:09:00 +0330

    feat: add wallet transaction relationships with customer and appointment

--- BarberHub.Domain/Entities/WalletTransaction.cs
+++ BarberHub.Domain/Entities/WalletTransaction.cs
@@ -3,12 +3,16 @@ using BarberHub.Domain.ValueObjects;
 
 namespace BarberHub.Domain.Entities;
 
-public class Wallet : BaseEntity
+public class WalletTransaction : BaseEntity
 {
     public Money Amount { get; private set; } = null!;
     public TransactionType TransactionType { get; private set; }
     public WalletTransactionReason WalletTransactionReason { get; private set; }
     public Money BalanceAfterTransaction { get; private set; } = null!;
     
-    
+    public long CustomerId { get; private set; }
+    public Customer Customer { get; private set; } = null!;
+    public long AppointmentId  { get; private set; }
+    public Appointment Appointment { get; private set; } = null!;
+
 }
