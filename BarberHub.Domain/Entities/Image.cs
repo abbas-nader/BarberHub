0c94d3152cc85afdda185a8b2f2922be8580c2ba
Abbas <abbasnader2025@gmail.com>
2026-08-03 00:15:09 +0330

    feat: add Image entity

---
+++ BarberHub.Domain/Entities/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace BarberHub.Domain.Entities;
+
+public class Image: BaseEntity
+{
+    public string ImageUrl { get; private set; } = null!;
+    public string? Caption { get; private set; }
+    
+    public long SalonId { get; private set; }
+    public Salon Salon { get; private set; } = null!;
+    
+    public long? BarberId { get; private set; }
+    public Barber? Barber { get; private set; } 
+}
