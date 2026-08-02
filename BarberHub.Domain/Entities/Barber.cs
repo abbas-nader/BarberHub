c4f6f9c3e73d188189a2c46165e9facd53fa85aa
Abbas <abbasnader2025@gmail.com>
2026-08-03 00:16:08 +0330

    feat: add navigation collection to Barber

--- BarberHub.Domain/Entities/Barber.cs
+++ BarberHub.Domain/Entities/Barber.cs
@@ -12,4 +12,6 @@ public class Barber : BaseEntity
     public IReadOnlyCollection<WorkSchedule> WorkSchedules { get; private set; } = null!;
     public IReadOnlyCollection<BarberService> BarberServices { get; private set; } = null!;
     public IReadOnlyCollection<Appointment> Appointments { get; private set; } = null!;
+    public IReadOnlyCollection<Image> Images { get; private set; } = null!;
+    
 }
