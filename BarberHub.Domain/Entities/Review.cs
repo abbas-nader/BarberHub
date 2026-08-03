210bdfa3d3c58ef52edf110ae47f3d49b839b2fb
Abbas <abbasnader2025@gmail.com>
2026-08-03 17:57:27 +0330

    feat: add Review entity

---
+++ BarberHub.Domain/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberHub.Domain.Entities;
+
+public class Review : BaseEntity
+{
+    public byte Rating { get; private set; }
+    public string Comment { get; private set; } = null!;
+    public bool IsApproved { get; private set; }
+    public string? Reply { get; private set; } = null!;
+    
+    public long CustomerId { get; private set; }
+    public long BarberId { get; private set; }
+    public long AppointmentId { get; private set; }
+    public long SalonId { get; private set; }
+}
