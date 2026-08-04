0126e0988eabd56e1b869a6c72e53a9ff4d0b442
Abbas <abbasnader2025@gmail.com>
2026-08-04 17:53:58 +0330

    feat: implement Image entity behavior

--- BarberHub.Domain/Entities/Image.cs
+++ BarberHub.Domain/Entities/Image.cs
@@ -1,13 +1,56 @@
-﻿namespace BarberHub.Domain.Entities;
+﻿using BarberHub.Domain.Exceptions;
 
-public class Image: BaseEntity
+namespace BarberHub.Domain.Entities;
+
+public class Image : BaseEntity
 {
     public string ImageUrl { get; private set; } = null!;
     public string? Caption { get; private set; }
-    
+
     public long SalonId { get; private set; }
     public Salon Salon { get; private set; } = null!;
-    
+
     public long? BarberId { get; private set; }
-    public Barber? Barber { get; private set; } 
+    public Barber? Barber { get; private set; }
+
+    private Image()
+    {
+    }
+
+    private Image(string imageUrl, string? caption, long salonId, long? barberId = null)
+    {
+        ImageUrl = imageUrl;
+        Caption = caption;
+        SalonId = salonId;
+        BarberId = barberId;
+    }
+
+    private static string CheckImageUrl(string imageUrl)
+    {
+        return string.IsNullOrWhiteSpace(imageUrl) ? throw new InvalidImageUrlException() : imageUrl;
+    }
+
+    public static Image CreateForSalon(long salonId, string imageUrl, string? caption, long creationBy)
+    {
+        var url = CheckImageUrl(imageUrl);
+        var image = new Image(url, caption, salonId);
+        image.SetCreationInfo(creationBy);
+        return image;
+    }
+
+    public static Image CreateForBarber(long barberId, long salonId, string imageUrl, string? caption, long creationBy)
+    {
+        var url = CheckImageUrl(imageUrl);
+        var image = new Image(url, caption, salonId, barberId);
+        image.SetCreationInfo(creationBy);
+        return image;
+    }
+
+    public void UpdateCaption(string? caption, long modifiedBy)
+    {
+        if (caption is { Length: > 500 })
+            throw new CaptionLengthExceededException();
+        Caption = caption;
+        MarkAsModified(modifiedBy);
+    }
 }
