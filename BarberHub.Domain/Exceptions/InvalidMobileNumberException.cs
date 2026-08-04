fcaf19dc9d235deace0fd5bef7a5d5f86bf45491
Abbas <abbasnader2025@gmail.com>
2026-08-04 19:44:38 +0330

    feat: add InvalidMobileNumberException

---
+++ BarberHub.Domain/Exceptions/InvalidMobileNumberException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarberHub.Domain.Exceptions
+{
+    public class InvalidMobileNumberException() : Exception("The mobile number cannot be null, empty, or invalid.")
+    {
+    }
+}
