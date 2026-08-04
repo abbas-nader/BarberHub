1d85b9c6fa111e197d66eca1b4e4cc684bc8190c
Abbas <abbasnader2025@gmail.com>
2026-08-04 19:44:17 +0330

    feat: add InvalidLastNameException

---
+++ BarberHub.Domain/Exceptions/InvalidLastNameException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarberHub.Domain.Exceptions
+{
+    public class InvalidLastNameException() : Exception("The last name cannot be null, empty, or invalid.")
+    {
+    }
+}
