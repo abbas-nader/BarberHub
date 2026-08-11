ec3a52a156caa707deab465243b0e9840e12a8c9
Abbas <abbasnader2025@gmail.com>
2026-08-11 16:51:55 +0330

    add SalonAdmin faunction

--- BarberHub.Domain/Entities/SalonAdmin.cs
+++ BarberHub.Domain/Entities/SalonAdmin.cs
@@ -3,8 +3,33 @@
 public class SalonAdmin : BaseEntity
 {
     public string FullName { get; private set; } = null!;
-    public string UserName { get;private set; } = null!;
-    public string PasswordHash { get;private set; } = null!;
-    
-    public long SalonId { get;private set; }
+    public string UserName { get; private set; } = null!;
+    public string PasswordHash { get; private set; } = null!;
+
+    public long SalonId { get; private set; }
+
+    private SalonAdmin()
+    {
+    }
+
+    public SalonAdmin(string fullName, string userName, string passwordHash, long salonId, long creationBy)
+    {
+        ValidateFullName(fullName);
+        
+        FullName = fullName;
+        UserName = userName;
+        PasswordHash = passwordHash;
+        SalonId = salonId;
+        Creation(creationBy);
+    }
+    private static void ValidateFullName(string fullName)
+    {
+        if (string.IsNullOrWhiteSpace(fullName) || fullName is { Length: > 100 })
+            throw new ();
+    }
+    private static void ValidateUserName(string userName)
+    {
+        if (string.IsNullOrWhiteSpace(userName) || userName is { Length: > 100 })
+            throw new ();
+    }
 }
