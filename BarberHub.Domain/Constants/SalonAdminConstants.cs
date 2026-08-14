ae6b5184f4592152b8fdd74710dfcd5fbc15a88c
abbas <abbasnader2025@gmail.com>
2026-08-14 12:02:09 +0330

    feat: refactor: add SalonAdmin constants for domain rules

---
+++ BarberHub.Domain/Constants/SalonAdminConstants.cs
@@ -0,0 +1,7 @@
+namespace BarberHub.Domain.Constants;
+
+public class SalonAdminConstants
+{
+    public const int  SalonAdminFullNameMaxLength = 100;
+    public const int  SalonAdminUserNameMaxLength = 100;
+}
