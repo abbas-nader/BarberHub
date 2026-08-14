b2f3149590fa600628d20ecfc38a81c4e3e64399
abbas <abbasnader2025@gmail.com>
2026-08-14 21:25:25 +0330

    Remove max length validation for SalonAdmin full name

--- BarberHub.Domain/Constants/SalonAdminConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace BarberHub.Domain.Constants;
-
-public class SalonAdminConstants
-{
-    public const int  SalonAdminFullNameMaxLength = 100;
-    public const int  SalonAdminUserNameMaxLength = 100;
-}
--- BarberHub.Domain/Exceptions/InvalidSalonAdminFullNameException.cs
+++
@@ -1,6 +0,0 @@
-namespace BarberHub.Domain.Exceptions;
-
-public class InvalidSalonAdminFullNameException()
-    : Exception("Invalid salon admin full name. Full name exceeds the maximum allowed length.")
-{
-}
