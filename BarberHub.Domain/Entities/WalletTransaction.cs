42a3f9cf97647b2d1582706bc49bbc94107b9824
abbas <abbasnader2025@gmail.com>
2026-08-16 00:53:30 +0330

    feat: add domain rules and methods

--- BarberHub.Domain/Entities/WalletTransaction.cs
+++ BarberHub.Domain/Entities/WalletTransaction.cs
@@ -1,4 +1,5 @@
 ﻿using BarberHub.Domain.Enums;
+using BarberHub.Domain.Exceptions.SharedExceptions;
 using BarberHub.Domain.ValueObjects;
 
 namespace BarberHub.Domain.Entities;
@@ -9,7 +10,30 @@ public class WalletTransaction : BaseEntity
     public TransactionType TransactionType { get; private set; }
     public WalletTransactionReason WalletTransactionReason { get; private set; }
     public Money BalanceAfterTransaction { get; private set; } = null!;
-    
+
     public long CustomerId { get; private set; }
-    public long AppointmentId  { get; private set; }
+    public long AppointmentId { get; private set; }
+
+    private WalletTransaction()
+    {
+    }
+
+    public WalletTransaction(Money amount, TransactionType transactionType,
+        WalletTransactionReason walletTransactionReason, Money balanceAfterTransaction, long customerId,
+        long appointmentId, long creationBy)
+    {
+        ValidateAmount(amount);
+        Amount = amount;
+        TransactionType = transactionType;
+        WalletTransactionReason = walletTransactionReason;
+        BalanceAfterTransaction = balanceAfterTransaction;
+        CustomerId = customerId;
+        AppointmentId = appointmentId;
+        Creation(creationBy);
+    }
+
+    private static void ValidateAmount(Money amount)
+    {
+        if (amount is null) throw new RequiredFieldException(nameof(amount));
+    }
 }
