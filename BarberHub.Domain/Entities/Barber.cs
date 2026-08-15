058f830cdbc32454f3cff383be582df1e778f762
abbas <abbasnader2025@gmail.com>
2026-08-15 21:44:26 +0330

    feat: add barber entity behaviors

--- BarberHub.Domain/Entities/Barber.cs
+++ BarberHub.Domain/Entities/Barber.cs
@@ -1,7 +1,14 @@
-﻿namespace BarberHub.Domain.Entities;
+﻿using BarberHub.Domain.Exceptions.SharedExceptions;
+
+namespace BarberHub.Domain.Entities;
 
 public class Barber : BaseEntity
 {
+    private readonly List<WorkSchedule> _workSchedules = [];
+    private readonly List<BarberService> _barberServices = [];
+    private readonly List<Appointment> _appointments = [];
+    private readonly List<File> _files = [];
+    private readonly List<Review> _reviews = [];
     public string FirstName { get; private set; } = null!;
     public string LastName { get; private set; } = null!;
     public string MobileNumber { get; private set; } = null!;
@@ -11,10 +18,92 @@ public class Barber : BaseEntity
     public bool IsActive { get; private set; }
 
     public long SalonId { get; private set; }
-    public IReadOnlyCollection<WorkSchedule> WorkSchedules { get; private set; } = null!;
-    public IReadOnlyCollection<BarberService> BarberServices { get; private set; } = null!;
-    public IReadOnlyCollection<Appointment> Appointments { get; private set; } = null!;
-    public IReadOnlyCollection<File> Files { get; private set; } = null!;
-    public IReadOnlyCollection<Review> Reviews { get; private set; } = null!;
-    
+    public IReadOnlyCollection<WorkSchedule> WorkSchedules => _workSchedules.AsReadOnly();
+    public IReadOnlyCollection<BarberService> BarberServices => _barberServices.AsReadOnly();
+    public IReadOnlyCollection<Appointment> Appointments => _appointments.AsReadOnly();
+    public IReadOnlyCollection<File> Files => _files.AsReadOnly();
+    public IReadOnlyCollection<Review> Reviews => _reviews.AsReadOnly();
+
+    private Barber()
+    {
+    }
+
+    public Barber(string firstName, string lastName, string mobileNumber, string userName, string passwordHash,
+        string? description, long salonId, long creationBy)
+    {
+        ValidateFirstName(firstName);
+        ValidateLastName(lastName);
+        ValidateMobileNumber(mobileNumber);
+        ValidateUserName(userName);
+        ValidatePasswordHash(passwordHash);
+        FirstName = firstName;
+        LastName = lastName;
+        MobileNumber = mobileNumber;
+        UserName = userName;
+        PasswordHash = passwordHash;
+        Description = description;
+        IsActive = true;
+        SalonId = salonId;
+        Creation(creationBy);
+    }
+
+    public void Update(string firstName, string lastName, string mobileNumber, string userName,
+        string passwordHash,
+        string? description, long salonId, long modifiedBy)
+    {
+        ValidateFirstName(firstName);
+        ValidateLastName(lastName);
+        ValidateMobileNumber(mobileNumber);
+        ValidateUserName(userName);
+        ValidatePasswordHash(passwordHash);
+        FirstName = firstName;
+        LastName = lastName;
+        MobileNumber = mobileNumber;
+        UserName = userName;
+        PasswordHash = passwordHash;
+        Description = description;
+        Modified(modifiedBy);
+    }
+
+    public void Activate(long modifiedBy)
+    {
+        IsActive = true;
+        Modified(modifiedBy);
+    }
+
+    public void Deactivate(long modifiedBy)
+    {
+        IsActive = false;
+        Modified(modifiedBy);
+    }
+
+    private static void ValidateFirstName(string firstName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new RequiredFieldException(nameof(firstName));
+    }
+
+    private static void ValidateLastName(string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new RequiredFieldException(nameof(lastName));
+    }
+
+    private static void ValidateMobileNumber(string mobileNumber)
+    {
+        if (string.IsNullOrWhiteSpace(mobileNumber))
+            throw new RequiredFieldException(nameof(mobileNumber));
+    }
+
+    private static void ValidateUserName(string userName)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+            throw new RequiredFieldException(nameof(userName));
+    }
+
+    private static void ValidatePasswordHash(string passwordHash)
+    {
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new RequiredFieldException(nameof(passwordHash));
+    }
 }
