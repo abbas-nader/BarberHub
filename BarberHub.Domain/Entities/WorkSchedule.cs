01d5462d2bb59d9e52d4538ce242171ce83e05df
abbas <abbasnader2025@gmail.com>
2026-08-16 00:53:52 +0330

    feat: add domain rules and methods

--- BarberHub.Domain/Entities/WorkSchedule.cs
+++ BarberHub.Domain/Entities/WorkSchedule.cs
@@ -1,10 +1,38 @@
-﻿namespace BarberHub.Domain.Entities;
+﻿using BarberHub.Domain.Exceptions;
+
+namespace BarberHub.Domain.Entities;
 
 public class WorkSchedule : BaseEntity
 {
-    public TimeOnly StartTime { get;private set; }
-    public TimeOnly EndTime { get;private set; }
-    public DayOfWeek DayOfWeek { get;private set; }
-    
-    public long BarberId { get;private set; }
+    public TimeOnly StartTime { get; private set; }
+    public TimeOnly EndTime { get; private set; }
+    public DayOfWeek DayOfWeek { get; private set; }
+
+    public long BarberId { get; private set; }
+
+    private WorkSchedule()
+    {
+    }
+
+    public WorkSchedule(TimeOnly startTime, TimeOnly endTime, DayOfWeek dayOfWeek, long barberId, long creationBy)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+        DayOfWeek = dayOfWeek;
+        BarberId = barberId;
+        Creation(creationBy);
+    }
+
+    public void Update(TimeOnly startTime, TimeOnly endTime, DayOfWeek dayOfWeek, long modifiedBy)
+    {
+        StartTime = startTime;
+        EndTime = endTime;
+        DayOfWeek = dayOfWeek;
+        Modified(modifiedBy);
+    }
+    public static void ValidateTimeRange(TimeOnly startTime, TimeOnly endTime)
+    {
+        if (endTime <= startTime)
+            throw new InvalidWorkScheduleTimeRangeException();
+    }
 }
