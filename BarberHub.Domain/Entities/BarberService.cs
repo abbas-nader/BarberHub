744ecfc0a9052505529376f1dd7fe823a06e3584
abbas <abbasnader2025@gmail.com>
2026-08-16 00:54:32 +0330

    feat: add domain rules and methods

--- BarberHub.Domain/Entities/BarberService.cs
+++ BarberHub.Domain/Entities/BarberService.cs
@@ -1,14 +1,28 @@
-﻿using BarberHub.Domain.ValueObjects;
+﻿using BarberHub.Domain.Enums;
+using BarberHub.Domain.Exceptions.SharedExceptions;
+using BarberHub.Domain.ValueObjects;
 
 namespace BarberHub.Domain.Entities;
 
 public class BarberService : BaseEntity
 {
-    public long  BarberId { get; private set; }
+    public long BarberId { get; private set; }
     public Barber Barber { get; private set; } = null!;
-    
+
     public long ServiceId { get; private set; }
     public Service Service { get; private set; } = null!;
 
     public Money Money { get; private set; } = null!;
+
+    private BarberService()
+    {
+    }
+
+    public BarberService(long barberId,long serviceId, Money money, long creationBy)
+    {
+        BarberId = barberId;
+        ServiceId = serviceId;
+        Money = money ?? throw new RequiredFieldException(nameof(money));
+        Creation(creationBy);
+    }
 }
