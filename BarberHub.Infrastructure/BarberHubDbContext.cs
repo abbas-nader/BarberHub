72a87c85b4f4c93cb27312264e60b242894cd844
abbas <abbasnader2025@gmail.com>
2026-08-16 17:25:08 +0330

    refactor: rename images to gallery and remove baseentity property

--- BarberHub.Infrastructure/BarberHubDbContext.cs
+++ BarberHub.Infrastructure/BarberHubDbContext.cs
@@ -6,12 +6,11 @@ namespace BarberHub.Infrastructure;
 
 public class BarberHubDbContext(DbContextOptions<BarberHubDbContext> options) : DbContext(options)
 {
-    public DbSet<BaseEntity> BaseEntities { get; set; }
     public DbSet<Appointment> Appointments { get; set; }
     public DbSet<Barber> Barbers { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<File> Files { get; set; }
-    public DbSet<Gallery> Images { get; set; }
+    public DbSet<Gallery> Galleries { get; set; }
     public DbSet<Review> Reviews { get; set; }
     public DbSet<Salon> Salons { get; set; }
     public DbSet<SalonAdmin> SalonAdmins { get; set; }
