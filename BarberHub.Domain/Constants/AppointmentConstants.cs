7f7090d8b45a53178629fbc74d45630495c94e4c
abbas <abbasnader2025@gmail.com>
2026-08-17 00:16:58 +0330

    feat: add appointment constants

---
+++ BarberHub.Domain/Constants/AppointmentConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace BarberHub.Domain.Constants;
+
+public class AppointmentConstants
+{
+    public const string DepositAmountValueColumnName  = "DepositAmountValue";
+    public const string DepositAmountValueColumnType  = "numeric(18,2)";
+    public const string DepositAmountCurrencyColumnName  = "DepositAmountCurrency";
+    public const string ServiceNameColumnName  = "ServiceName";
+    public const string ServiceDurationColumnName  = "ServiceDuration";
+    public const string ServicePriceValueColumnName  = "ServicePriceValue";
+    public const string ServicePriceColumnType  = "numeric(18,2)";
+    public const string ServicePriceCurrencyColumnName  = "ServicePriceCurrency";
+}
