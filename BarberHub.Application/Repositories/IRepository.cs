9109aa5c9308ba141f51bb6125d71e0ba856dadc
abbas <abbasnader2025@gmail.com>
2026-08-16 16:49:49 +0330

    feat: add repository interface

---
+++ BarberHub.Application/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Application.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(long id,  CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    void Update(TEntity entity);
+    void DeleteAsync(TEntity entity);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
