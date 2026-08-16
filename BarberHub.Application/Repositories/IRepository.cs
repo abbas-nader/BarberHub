fcebf1efb7c7401d2bcf7e6069bd9666504985ea
abbas <abbasnader2025@gmail.com>
2026-08-16 17:30:48 +0330

    refactor: remove delete method from repository

--- BarberHub.Application/Repositories/IRepository.cs
+++ BarberHub.Application/Repositories/IRepository.cs
@@ -6,6 +6,5 @@ public interface IRepository<TEntity> where TEntity : class
     Task<IReadOnlyCollection<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
     Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
     void Update(TEntity entity);
-    void DeleteAsync(TEntity entity);
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BaseRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BaseRepository.cs
@@ -3,26 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 
-public abstract class BaseRepository<TEntity>(BarberHubDbContext barberHubDbContext) : IRepository<TEntity> where TEntity : class
+public abstract class BaseRepository<TEntity>(BarberHubDbContext barberHubDbContext)
+    : IRepository<TEntity> where TEntity : class
 {
     protected readonly BarberHubDbContext BarberHubDbContext = barberHubDbContext;
-    private readonly DbSet<TEntity> _dbSet =  barberHubDbContext.Set<TEntity>();
+    private readonly DbSet<TEntity> _dbSet = barberHubDbContext.Set<TEntity>();
 
     public async Task<TEntity?> GetByIdAsync(long id, CancellationToken cancellationToken = default)
         => await _dbSet.FindAsync([id], cancellationToken);
 
     public async Task<IReadOnlyCollection<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
-    => await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
+        => await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
 
     public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
-    => await  _dbSet.AddAsync(entity, cancellationToken);
+        => await _dbSet.AddAsync(entity, cancellationToken);
 
     public void Update(TEntity entity)
-    => _dbSet.Update(entity);
-
-    public void DeleteAsync(TEntity entity)
-    => _dbSet.Remove(entity);
+        => _dbSet.Update(entity);
 
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    => BarberHubDbContext.SaveChangesAsync(cancellationToken);
+        => BarberHubDbContext.SaveChangesAsync(cancellationToken);
 }
