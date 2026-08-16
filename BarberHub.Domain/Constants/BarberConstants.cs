a9e452ad7765276aef05957be8e591ad1626d70d
abbas <abbasnader2025@gmail.com>
2026-08-17 00:17:34 +0330

    feat: add barber constants

---
+++ BarberHub.Domain/Constants/BarberConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Domain.Constants;
+
+public class BarberConstants
+{
+    public const int FirstNameMaxLength = 50;
+    public const int LastNameMaxLength = 50;
+    public const int MobileMaxLength = 11;
+    public const int UserNameMaxLength = 50;
+    public const int PasswordHashMaxLength = 255;
+    public const int DescriptionMaxLength = 255;
+}
