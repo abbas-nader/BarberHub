6f528dad806599766335b7ab071a100fb024af07
abbas <abbasnader2025@gmail.com>
2026-08-17 12:59:56 +0330

    feat: add customer constants

---
+++ BarberHub.Domain/Constants/CustomerConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace BarberHub.Domain.Constants;
+
+public class CustomerConstants
+{
+    public const string FirstNameColumnType = "varchar";
+    public const int FirstNameMaxLength = 50;
+    public const string LastNameColumnType = "varchar";
+    public const int LastNameMaxLength = 50;
+    public const string MobileNumberColumnType = "varchar";
+    public const int MobileNumberMaxLength = 11;
+    public const string UsernameColumnType = "varchar";
+    public const int UsernameMaxLength = 50;
+    public const string PasswordColumnType = "varchar";
+    public const int PasswordMaxLength = 255;
+}
