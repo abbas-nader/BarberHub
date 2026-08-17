265b0857efe26082399d9cce5e4795bbd21157c6
abbas <abbasnader2025@gmail.com>
2026-08-17 11:45:20 +0330

    feat: configure barber service relationship

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/BarberServiceConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using BarberHub.Domain.Constants;
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class BarberServiceConfiguration : IEntityTypeConfiguration<BarberService>
+{
+    public void Configure(EntityTypeBuilder<BarberService> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.ComplexProperty(x => x.Money, money =>
+            {
+                money.Property(m => m.Value)
+                    .HasColumnName(BarberServiceConstants.PriceValueColumnName)
+                    .HasColumnType(BarberServiceConstants.PriceValueColumnType)
+                    .IsRequired();
+                money.Property(m => m.Currency)
+                    .HasColumnName(BarberServiceConstants.PriceCurrencyColumnName)
+                    .IsRequired();
+            }
+        );
+        builder.HasOne<Service>()
+            .WithMany()
+            .HasForeignKey(x => x.ServiceId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne<Barber>()
+            .WithMany()
+            .HasForeignKey(x => x.BarberId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
