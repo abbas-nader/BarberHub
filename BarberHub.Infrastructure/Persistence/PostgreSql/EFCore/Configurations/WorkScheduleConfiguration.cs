b040053d98ed63d1c64ba3935b82d7ae8f4ed580
abbas <abbasnader2025@gmail.com>
2026-08-17 18:55:41 +0330

    feat: add WorkSchedule configuration

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/WorkScheduleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class WorkScheduleConfiguration  :IEntityTypeConfiguration<WorkSchedule>
+{
+    public void Configure(EntityTypeBuilder<WorkSchedule> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x=>x.StartTime)
+            .IsRequired();
+        builder.Property(x=>x.EndTime)
+            .IsRequired();
+        builder.Property(x=>x.DayOfWeek)
+            .IsRequired();
+        
+       builder.HasOne<Barber>()
+           .WithMany(x=>x.WorkSchedules)
+           .HasForeignKey(x=>x.BarberId)
+           .OnDelete(DeleteBehavior.Cascade);
+    }
+}
