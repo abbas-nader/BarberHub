cdb77d15d6f0826de4022bf6c2f748b90b56c04b
abbas <abbasnader2025@gmail.com>
2026-08-17 11:45:30 +0330

    feat: add barber service constants

---
+++ BarberHub.Domain/Constants/BarberServiceConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Domain.Constants;
+
+public class BarberServiceConstants
+{
+    public const string PriceValueColumnName = "PriceValue";
+    public const string PriceValueColumnType = "numeric(18,2)";
+    public const string PriceCurrencyColumnName = "PriceCurrency";
+}
