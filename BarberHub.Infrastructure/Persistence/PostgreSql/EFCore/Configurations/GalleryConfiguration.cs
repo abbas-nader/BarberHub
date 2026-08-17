4a13dd5cf0eaf2e07f43a4b944bc1888d9912964
abbas <abbasnader2025@gmail.com>
2026-08-17 18:51:56 +0330

    feat: add Gallery configuration

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/GalleryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using BarberHub.Domain.Constants;
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class GalleryConfiguration : IEntityTypeConfiguration<Gallery>
+{
+    public void Configure(EntityTypeBuilder<Gallery> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Caption)
+            .HasMaxLength(GalleryConstants.CaptionMaxLength);
+        
+      builder.HasOne(x=>x.Salon)
+          .WithMany(x=> x.Galleries)
+          .HasForeignKey(x=>x.SalonId)
+          .OnDelete(DeleteBehavior.Restrict);
+      builder.HasOne(x=>x.Barber)
+          .WithMany(x=>x.Galleries)
+          .HasForeignKey(x=>x.BarberId)
+          .OnDelete(DeleteBehavior.Restrict);
+      builder.HasOne(x=>x.File)
+          .WithOne()
+          .HasForeignKey<Gallery>(x=>x.FileId)
+          .OnDelete(DeleteBehavior.Restrict);
+      
+    }
+}
