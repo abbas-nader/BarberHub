a80b8cb9c73fa0f6601f8400286cb93eb27f7236
abbas <abbasnader2025@gmail.com>
2026-08-17 18:55:18 +0330

    feat: add WalletTransaction constants

---
+++ BarberHub.Domain/Constants/WalletTransactionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Domain.Constants;
+
+public class WalletTransactionConstants
+{
+    public const string AmountValueColumnName = "AmountValue";
+    public const string AmountValueColumnTypeName = "decimal(18,2)";
+    public const string AmountCurrencyColumnName = "AmountCurrency";
+    public const string BalanceAfterTransactionValueColumnName = "BalanceAfterTransactionValue";
+    public const string BalanceAfterTransactionValueColumnTypeName = "decimal(18,2)";
+    public const string BalanceAfterTransactionCurrencyColumnName = "BalanceAfterTransactionCurrency";
+}
