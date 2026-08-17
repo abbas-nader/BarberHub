cac5ec0dcf3625d72b337248d21d86644bbc84e0
abbas <abbasnader2025@gmail.com>
2026-08-17 18:51:15 +0330

    feat: add constants to Constants file

--- BarberHub.Domain/Constants/FileConstants.cs
+++ BarberHub.Domain/Constants/FileConstants.cs
@@ -3,4 +3,8 @@
 public static class FileConstants
 {
     public const long SizeMinLength = 0;
+    public const int FileNameMaxLength = 255;
+    public const int OriginFileNameMaxLength = 255;
+    public const int UrlMaxLength = 2000;
+    public const int ContentTypeMaxLength = 100;
 }
