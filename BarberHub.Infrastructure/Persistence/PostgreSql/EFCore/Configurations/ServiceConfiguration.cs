e635d3553bd464b114da9a3b99b50ff5df25757f
abbas <abbasnader2025@gmail.com>
2026-08-17 18:54:21 +0330

    feat: add Service configuration

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BarberHub.Domain.Constants;
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+{
+    public void Configure(EntityTypeBuilder<Service> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(ServiceConstants.NameMaxLength)
+            .IsRequired();
+        builder.Property(x => x.Description)
+            .HasMaxLength(ServiceConstants.DescriptionMaxLength);
+        builder.Property(x => x.Duration)
+            .IsRequired();
+    }
+}
