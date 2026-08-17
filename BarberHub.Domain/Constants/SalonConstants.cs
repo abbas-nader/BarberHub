d00b20d17683c1bc39295d26c60558f0d85d9214
abbas <abbasnader2025@gmail.com>
2026-08-17 18:53:47 +0330

    feat: add Salon constants

--- BarberHub.Domain/Constants/SalonConstants.cs
+++ BarberHub.Domain/Constants/SalonConstants.cs
@@ -5,4 +5,13 @@ namespace BarberHub.Domain.Constants;
 public class SalonConstants
 {
     public const int DescriptionMaxLength = 1000;
+    public const int NameMaxLength = 100;
+    public const int AddressMaxLength = 500;
+    public const int CityMaxLength = 50;
+    public const int PhoneNumberMaxLength = 11;
+    public const string DepositAmountValueColumnName = "DepositAmountValue";
+    public const string DepositAmountColumnType = "numeric(18,2)";
+    public const string DepositAmountCurrencyCodeColumnName = "DepositAmountCurrency";
+    
+    
 }
