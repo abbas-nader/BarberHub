a529822ea59ca6b655274b9b1dab6d5eef8d9094
abbas <abbasnader2025@gmail.com>
2026-08-17 18:55:14 +0330

    feat: add WalletTransaction configuration

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/WalletTransactionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using BarberHub.Domain.Constants;
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class WalletTransactionConfiguration : IEntityTypeConfiguration<WalletTransaction>
+{
+    public void Configure(EntityTypeBuilder<WalletTransaction> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.ComplexProperty(x => x.Amount, money =>
+        {
+            money.Property(x=>x.Value)
+                .HasColumnName(WalletTransactionConstants.AmountValueColumnName)
+                .HasColumnType(WalletTransactionConstants.AmountValueColumnTypeName)
+                .IsRequired();
+            money.Property(x => x.Currency)
+                .HasColumnName(WalletTransactionConstants.AmountCurrencyColumnName)
+                .IsRequired();
+        });
+        builder.ComplexProperty(x => x.BalanceAfterTransaction, money =>
+        {
+            money.Property(x=>x.Value)
+                .HasColumnName(WalletTransactionConstants.BalanceAfterTransactionValueColumnName)
+                .HasColumnType(WalletTransactionConstants.BalanceAfterTransactionValueColumnTypeName)
+                .IsRequired();
+            money.Property(x => x.Currency)
+                .HasColumnName(WalletTransactionConstants.BalanceAfterTransactionCurrencyColumnName)
+                .IsRequired();
+        });
+       
+    }
+}
