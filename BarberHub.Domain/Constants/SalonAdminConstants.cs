d8284343f3ef9fc1e53a2d1f563c8829f0ecd211
abbas <abbasnader2025@gmail.com>
2026-08-17 18:53:24 +0330

    feat: add SalonAdmin constants

---
+++ BarberHub.Domain/Constants/SalonAdminConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Domain.Constants;
+
+public class SalonAdminConstants
+{
+    public const int FirstNameMaxLength = 50;
+    public const int LastNameMaxLength = 50;
+    public const int UsernameMaxLength = 50;
+    public const int PasswordMaxLength = 255;
+    public const int PhoneNumberMaxLength = 11;
+}
