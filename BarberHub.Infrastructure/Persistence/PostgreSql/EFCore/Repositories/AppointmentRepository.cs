ce7801d64d74591031dc686f67e3de8f665c2bf0
abbas <abbasnader2025@gmail.com>
2026-08-18 05:56:09 +0330

    feat: add appointment repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/AppointmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Entities;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class AppointmentRepository(BarberHubDbContext context)
+    : BaseRepository<Appointment>(context), IAppointmentRepository
+{
+}
