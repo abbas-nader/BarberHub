94277b73cc7969f25888e6c93d8c284d29e71b58
abbas <abbasnader2025@gmail.com>
2026-08-18 15:52:42 +0330

    feat: add dependency injection and PostgreSQL connection string

---
+++ BarberHub.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using BarberHub.Infrastructure.Persistence.PostgreSql.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BarberHub.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BarberHubDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("Postgres")));
+    }
+}
