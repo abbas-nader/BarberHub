61d0971b356f5c575dc15307897a323da2fee4e5
abbas <abbasnader2025@gmail.com>
2026-08-18 05:58:24 +0330

    feat: add work schedule repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/WorkScheduleRepository.cs
@@ -0,0 +1,9 @@
+﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Entities;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class WorkScheduleRepository(BarberHubDbContext context)
+    : BaseRepository<WorkSchedule>(context), IWorkScheduleRepository
+{
+}
