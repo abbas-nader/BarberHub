ed5afb28ce73c265e9197ac26b5063cd8acf3de2
abbas <abbasnader2025@gmail.com>
2026-08-18 05:57:00 +0330

    feat: add file repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/FileRepository.cs
@@ -0,0 +1,8 @@
+﻿using BarberHub.Application.Repositories;
+using File = BarberHub.Domain.Entities.File;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class FileRepository(BarberHubDbContext context) : BaseRepository<File>(context), IFileRepository
+{
+}
