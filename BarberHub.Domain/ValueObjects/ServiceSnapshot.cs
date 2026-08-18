2954ca7ceee54636fa695e6063c9d07ce2ab9a38
abbas <abbasnader2025@gmail.com>
2026-08-18 15:53:00 +0330

    refactor: add private constructor for EF Core

--- BarberHub.Domain/ValueObjects/ServiceSnapshot.cs
+++ BarberHub.Domain/ValueObjects/ServiceSnapshot.cs
@@ -3,13 +3,17 @@ using BarberHub.Domain.Exceptions.SharedExceptions;
 
 namespace BarberHub.Domain.ValueObjects;
 
-public sealed record ServiceSnapshot
+public abstract record ServiceSnapshot
 {
-    public string ServiceName { get; }
-    public TimeSpan ServiceDuration { get; }
-    public Money ServicePrice { get; }
+    public string ServiceName { get; private set; } = null!;
+    public TimeSpan ServiceDuration { get; private set; }
+    public Money ServicePrice { get; private set; } = null!;
 
-    public ServiceSnapshot(string serviceName, TimeSpan serviceDuration, Money servicePrice)
+    private ServiceSnapshot()
+    {
+    }
+
+    protected ServiceSnapshot(string serviceName, TimeSpan serviceDuration, Money servicePrice)
     {
         if (serviceDuration.Ticks <= 0) throw new InvalidServiceDurationSnapshotException();
         if (string.IsNullOrWhiteSpace(serviceName))
