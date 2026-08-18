8b4b194d4ba8f8bb2722d62702f3ba79d33a786a
abbas <abbasnader2025@gmail.com>
2026-08-18 05:57:15 +0330

    feat: add gallery repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/GalleryRepository.cs
@@ -0,0 +1,8 @@
+﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Entities;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class GalleryRepository(BarberHubDbContext context) : BaseRepository<Gallery>(context), IGalleryRepository
+{
+}
