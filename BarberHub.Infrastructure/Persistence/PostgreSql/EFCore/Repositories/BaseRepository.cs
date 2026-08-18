c9d8e9d2f4da62dfe0899bea597a5a3e5fd9d982
abbas <abbasnader2025@gmail.com>
2026-08-18 14:04:04 +0330

    feat: implement paged query in base repository

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BaseRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
@@ -14,6 +15,18 @@ public abstract class BaseRepository<TEntity>(BarberHubDbContext barberHubDbCont
 
     public async Task<IReadOnlyCollection<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
         => await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
+
+    public async Task<PaginatedResult<TEntity>> GetPagedAsync(int pageNumber, int pageSize,
+        CancellationToken cancellationToken = default)
+    {
+        var totalCount = _dbSet.Count();
+        var items = await _dbSet.AsNoTracking()
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new  PaginatedResult<TEntity>(items, pageNumber, pageSize, totalCount);
+    }
 
     public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
         => await _dbSet.AddAsync(entity, cancellationToken);
