23ebb1f0f09ac59bd95a9f16f96f2eddc2346a0d
abbas <abbasnader2025@gmail.com>
2026-08-18 14:03:56 +0330

    feat: add paged query to repository interface

--- BarberHub.Application/Repositories/IRepository.cs
+++ BarberHub.Application/Repositories/IRepository.cs
@@ -1,9 +1,15 @@
-﻿namespace BarberHub.Application.Repositories;
+﻿using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Application.Repositories;
 
 public interface IRepository<TEntity> where TEntity : class
 {
-    Task<TEntity?> GetByIdAsync(long id,  CancellationToken cancellationToken = default);
+    Task<TEntity?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
     Task<IReadOnlyCollection<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task<PaginatedResult<TEntity>> GetPagedAsync(int pageNumber, int pageSize,
+        CancellationToken cancellationToken = default);
+
     Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
     void Update(TEntity entity);
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
