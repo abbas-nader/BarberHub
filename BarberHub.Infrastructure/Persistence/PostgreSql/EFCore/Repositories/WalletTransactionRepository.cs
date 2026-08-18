de6b93952c58f2f7e0194af61b2a649aa886f1dc
abbas <abbasnader2025@gmail.com>
2026-08-18 05:58:40 +0330

    feat: add wallet transaction repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/WalletTransactionRepository.cs
@@ -0,0 +1,9 @@
+﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Entities;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class WalletTransactionRepository(BarberHubDbContext context)
+    : BaseRepository<WalletTransaction>(context), IWalletTransactionRepository
+{
+}
