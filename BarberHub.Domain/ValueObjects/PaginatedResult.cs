a5f2225fc08dc906b1982baea1160c99b2030dbb
abbas <abbasnader2025@gmail.com>
2026-08-18 14:03:02 +0330

    feat: add paginated result

---
+++ BarberHub.Domain/ValueObjects/PaginatedResult.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using BarberHub.Domain.Exceptions;
+using BarberHub.Domain.Exceptions.SharedExceptions;
+
+namespace BarberHub.Domain.ValueObjects;
+
+public record PaginatedResult<T>
+{
+    public IReadOnlyCollection<T> Items { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public int TotalCount { get; init; }
+
+    public PaginatedResult(IReadOnlyCollection<T> items, int pageNumber, int pageSize, int totalCount)
+    {
+        ValidatePageNumber(pageNumber);
+        ValidatePageSize(pageSize);
+        ValidateTotalCount(totalCount);
+        Items = items ?? throw new RequiredFieldException(nameof(items));
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+
+    private static void ValidatePageNumber(int pageNumber)
+    {
+        if (pageNumber < 1) throw new InvalidPageNumberException();
+    }
+
+    private static void ValidatePageSize(int pageSize)
+    {
+        if (pageSize < 1) throw new InvalidPageSizeException();
+    }
+
+    private static void ValidateTotalCount(int pageCount)
+    {
+        if (pageCount < 0) throw new InvalidTotalCountException();
+    }
+}
