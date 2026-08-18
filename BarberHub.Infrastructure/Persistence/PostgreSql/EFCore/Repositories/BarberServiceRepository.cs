15dbc1c900bf89639cbf366a214e0950828a276e
abbas <abbasnader2025@gmail.com>
2026-08-18 05:56:38 +0330

    feat: add barber service repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BarberServiceRepository.cs
@@ -0,0 +1,9 @@
+﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Entities;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class BarberServiceRepository(BarberHubDbContext context)
+    : BaseRepository<BarberService>(context), IBarberServiceRepository
+{
+}
