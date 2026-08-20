902db375b1ecf589f70185602cb8a16f9d0a4c94
abbas <abbasnader2025@gmail.com>
2026-08-20 21:03:38 +0330

    feat: add barber dto

---
+++ BarberHub.Application/DTOs/BarberDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Application.DTOs;
+
+public record BarberDto(
+    long Id,
+    string FirstName,
+    string LastName,
+    string MobileNumber,
+    string? Description,
+    bool IsActive
+);
