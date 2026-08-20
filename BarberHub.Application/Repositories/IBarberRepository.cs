2300a65edbbd79db798552fea12fb7b4d3e62a15
abbas <abbasnader2025@gmail.com>
2026-08-20 21:04:00 +0330

    feat: add ExistsByUserNameAsync to barber repository interface

--- BarberHub.Application/Repositories/IBarberRepository.cs
+++ BarberHub.Application/Repositories/IBarberRepository.cs
@@ -4,4 +4,5 @@ namespace BarberHub.Application.Repositories;
 
 public interface IBarberRepository:IRepository<Barber>
 {
+    Task<bool> ExistsByUserNameAsync(string userName, CancellationToken cancellationToken = default);
 }
