52ae0afce8b5c381925d0c5bba4d3b842ea2f82b
abbas <abbasnader2025@gmail.com>
2026-08-20 21:04:16 +0330

    feat: add password hasher interface

---
+++ BarberHub.Application/Security/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Application.Security;
+
+public interface IPasswordHasher
+{
+    string Hash(string password);
+    bool Verify(string plainPassword, string hashedPassword);
+    
+}
