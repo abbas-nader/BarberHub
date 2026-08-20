f2e8fe8e78a08e1718f906f1d780931cf8bccbd0
abbas <abbasnader2025@gmail.com>
2026-08-20 21:04:08 +0330

    feat: implement ExistsByUserNameAsync in barber repository

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BarberRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BarberRepository.cs
@@ -1,8 +1,13 @@
 ﻿using BarberHub.Application.Repositories;
 using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 
 public class BarberRepository(BarberHubDbContext context) : BaseRepository<Barber>(context), IBarberRepository
 {
+    public async Task<bool> ExistsByUserNameAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        return await BarberHubDbContext.Barbers.AnyAsync(x => x.UserName == userName, cancellationToken: cancellationToken);
+    }
 }
