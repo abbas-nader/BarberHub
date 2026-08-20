3af792834bceef3db40a3c7758bcc3f709276a13
abbas <abbasnader2025@gmail.com>
2026-08-20 21:04:33 +0330

    feat: implement password hasher

---
+++ BarberHub.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using BarberHub.Application.Security;
+using BarberHub.Domain.Exceptions.SharedExceptions;
+
+namespace BarberHub.Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return string.IsNullOrWhiteSpace(password)
+            ? throw new RequiredFieldException(nameof(password))
+            : BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string plainPassword, string hashedPassword)
+    {
+        return string.IsNullOrWhiteSpace(plainPassword) ? throw new RequiredFieldException(nameof(plainPassword)) :
+            string.IsNullOrWhiteSpace(hashedPassword) ? throw new RequiredFieldException(nameof(hashedPassword)) :
+            BCrypt.Net.BCrypt.Verify(plainPassword, hashedPassword);
+    }
+}
