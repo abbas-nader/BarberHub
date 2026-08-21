8fa5994b820e8aabec78823042431f108c676e3c
abbas <abbasnader2025@gmail.com>
2026-08-21 20:58:52 +0330

    refactor: add update barber validation messages

---
+++ BarberHub.Api/Validators/Messages/Barber/UpdateBarberValidationMessages.cs
@@ -0,0 +1,29 @@
+﻿namespace BarberHub.Api.Validators.Messages.Barber;
+
+public static class UpdateBarberValidationMessages
+{
+    public const string IdProperty = "Id";
+    
+    public const string FirstNameProperty = "FirstName";
+    
+    public const string LastNameProperty = "LastName";
+    
+    public const string MobileNumberProperty = "MobileNumber";
+    public const string MobileNumberInvalidFormat = "Mobile number format is invalid.";
+    
+    public const string UsernameProperty = "Username";
+    public const string UsernameInvalidFormat = "Username format is invalid.";
+    
+    public const string PasswordProperty = "Password";
+    
+    public const string DescriptionProperty = "Description";
+    
+    public const string SalonIdInvalid = "Salon id must be a positive number.";
+    
+    
+    public static string PropertyRequired(string propertyName) => $"{propertyName} is required.";
+    public static string PropertyMaxLength(string propertyName) =>
+        $"{propertyName} exceeds the maximum allowed length.";
+    public static string PropertyMinLength(string propertyName) =>
+        $"{propertyName} is shorter than the minimum required length.";
+}
