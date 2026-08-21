f2dc4fb95982e9afae6542cb711eca1980e249c4
abbas <abbasnader2025@gmail.com>
2026-08-21 13:52:40 +0330

    feat: register barber service in application dependency injection

---
+++ BarberHub.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using BarberHub.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BarberHub.Application;
+
+public static class DependencyInjection
+{
+    public static void AddApplications(this IServiceCollection services)
+    {
+        services.AddScoped<BarberService>();
+    }
+}
