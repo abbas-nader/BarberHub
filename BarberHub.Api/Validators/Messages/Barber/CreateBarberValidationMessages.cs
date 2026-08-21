7691cca13a0e385b961c5823abe8a5394463e266
abbas <abbasnader2025@gmail.com>
2026-08-21 20:58:33 +0330

    feat: add create barber validation messages

---
+++ BarberHub.Api/Validators/Messages/Barber/CreateBarberValidationMessages.cs
@@ -0,0 +1,27 @@
+﻿namespace BarberHub.Api.Validators.Messages.Barber;
+
+public static class CreateBarberValidationMessages
+{
+    public const string FirstNameProperty = "FirstName";
+    
+    public const string LastNameProperty = "LastName";
+    
+    public const string MobileNumberProperty = "MobileNumber";
+    public const string MobileNumberInvalidFormat = "Mobile number format is invalid.";
+    
+    public const string UsernameProperty = "Username";
+    public const string UsernameInvalidFormat = "Username format is invalid.";
+    
+    public const string PasswordProperty = "Password";
+    
+    public const string DescriptionProperty = "Description";
+    
+    public const string SalonIdInvalid = "Salon id must be a positive number.";
+    
+    
+    public static string PropertyRequired(string propertyName) => $"{propertyName} is required.";
+    public static string PropertyMaxLength(string propertyName) =>
+        $"{propertyName} exceeds the maximum allowed length.";
+    public static string PropertyMinLength(string propertyName) =>
+        $"{propertyName} is shorter than the minimum required length.";
+}
