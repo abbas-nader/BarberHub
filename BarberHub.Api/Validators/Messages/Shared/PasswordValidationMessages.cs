5bbb2d9aa09a8f117797c001edcb96b270f4e156
abbas <abbasnader2025@gmail.com>
2026-08-21 21:00:23 +0330

    refactor: add password validation constants

---
+++ BarberHub.Api/Validators/Messages/Shared/PasswordValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace BarberHub.Api.Validators.Messages.Shared;
+
+public static class PasswordValidationMessages
+{
+    public const int MinLength = 8;
+    public const int MaxLength = 72;
+}
