3ab195f3168a5acc2a943a1f9f49a93d97db0dc9
abbas <abbasnader2025@gmail.com>
2026-08-21 13:51:51 +0330

    feat: add barber contract mapper extensions

---
+++ BarberHub.Api/Mappers/BarberContractMapper.cs
@@ -0,0 +1,39 @@
+﻿using BarberHub.Api.Contracts.Barber;
+using BarberHub.Application.DTOs;
+
+namespace BarberHub.Api.Mappers;
+
+public static class BarberContractMapper
+{
+    public static BarberResponse ToResponse(this BarberDto dto) =>
+        new(
+            dto.Id,
+            dto.FirstName,
+            dto.LastName,
+            dto.MobileNumber,
+            dto.Description,
+            dto.IsActive
+        );
+
+    public static CreateBarberRequest ToDto(this CreateBarberDto dto) =>
+        new(
+            dto.FirstName,
+            dto.LastName,
+            dto.MobileNumber,
+            dto.Username,
+            dto.Password,
+            dto.Description,
+            dto.SalonId
+        );
+
+    public static UpdateBarberRequest ToDto(this UpdateBarberDto dto) =>
+        new(
+            dto.Id,
+            dto.FirstName,
+            dto.LastName,
+            dto.MobileNumber,
+            dto.Username,
+            dto.Password,
+            dto.Description
+        );
+}
