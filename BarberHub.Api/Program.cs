5b45de9eda3478e101b9c9a9d36c82440ee21754
abbas <abbasnader2025@gmail.com>
2026-08-21 20:59:37 +0330

    feat: register global validation filter

--- BarberHub.Api/Program.cs
+++ BarberHub.Api/Program.cs
@@ -1,5 +1,7 @@
+using BarberHub.Api.Contracts.Barber;
 using BarberHub.Application;
 using BarberHub.Infrastructure;
+using FluentValidation;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplications();
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
