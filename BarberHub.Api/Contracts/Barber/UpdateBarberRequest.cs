90fd82463a6f682132ffa66b42eefd82605db8a4
abbas <abbasnader2025@gmail.com>
2026-08-21 13:51:13 +0330

    feat: add barber update request contract

---
+++ BarberHub.Api/Contracts/Barber/UpdateBarberRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Api.Contracts.Barber;
+
+public record UpdateBarberRequest(
+    long Id,
+    string FirstName,
+    string LastName,
+    string MobileNumber,
+    string Username,
+    string? Password,
+    string? Description
+);
