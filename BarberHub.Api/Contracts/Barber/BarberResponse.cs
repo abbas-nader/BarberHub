5dc3b32c2b7107d37d83c04ef0a1ac773b43ac6f
abbas <abbasnader2025@gmail.com>
2026-08-21 13:50:51 +0330

    feat: add barber response contract

---
+++ BarberHub.Api/Contracts/Barber/BarberResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Api.Contracts.Barber;
+
+public record BarberResponse(
+    long Id,
+    string FirstName,
+    string LastName,
+    string MobileNumber,
+    string? Description,
+    bool IsActive
+    );
