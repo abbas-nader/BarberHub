94db1bd1dae480925777583047fed9a3242bff0c
abbas <abbasnader2025@gmail.com>
2026-08-22 21:47:03 +0330

    feat: add base controller

---
+++ BarberHub.Api/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarberHub.Api.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/")]
+public abstract class BaseController : ControllerBase
+{
+}
