03b615c12654abda87f917adb10e76db615dd5ff
abbas <abbasnader2025@gmail.com>
2026-08-22 21:51:52 +0330

    feat: add exception log contract mappings

---
+++ BarberHub.Api/Mappers/ExceptionLogContractMappings.cs
@@ -0,0 +1,20 @@
+﻿using BarberHub.Api.Contracts.ExceptionLog;
+using BarberHub.Application.DTOs;
+
+namespace BarberHub.Api.Mappers;
+
+public static class ExceptionLogContractMappings
+{
+    public static ExceptionLogResponse ToResponse(this ExceptionLogDto exceptionLogDto)
+        => new(
+            exceptionLogDto.Id,
+            exceptionLogDto.ExceptionType,
+            exceptionLogDto.Message,
+            exceptionLogDto.StackTrace,
+            exceptionLogDto.Source,
+            exceptionLogDto.RequestPath,
+            exceptionLogDto.RequestMethod,
+            exceptionLogDto.StatusCode,
+            exceptionLogDto.CreatedAt
+        );
+}
