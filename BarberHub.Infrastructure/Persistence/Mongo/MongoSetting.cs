bf34554a56bcfca97a9f0c893225aaa10b3f83fa
abbas <abbasnader2025@gmail.com>
2026-08-22 21:49:12 +0330

    fix: update MongoDB database name

--- BarberHub.Infrastructure/Persistence/Mongo/MongoSetting.cs
+++ BarberHub.Infrastructure/Persistence/Mongo/MongoSetting.cs
@@ -5,6 +5,6 @@ public class MongoSetting
     public const string SectionName = "Mongo";
     
     public string ConnectionString { get; set; } = "mongodb://localhost:27017";
-    public string DatabaseName { get; set; } = "BarberHub";
+    public string DatabaseName { get; set; } = "BarberHubDb";
     public string ExceptionLogCollection { get; set; } = "exception_logs";
 }
