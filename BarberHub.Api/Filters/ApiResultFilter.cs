db26025b7f6e892deddaa3d5f6c294f5f8e2d98b
abbas <abbasnader2025@gmail.com>
2026-08-22 21:41:09 +0330

    feat: implement ApiResult filter

---
+++ BarberHub.Api/Filters/ApiResultFilter.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BarberHub.Api.Filters;
+
+public class ApiResultFilter(IServiceProvider serviceProvider) : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var errors = new Dictionary<string, string[]>();
+
+        foreach (var parameter in context.ActionDescriptor.Parameters)
+        {
+            if (!context.ActionArguments.TryGetValue(parameter.Name, out var argument) || argument is null)
+                continue;
+            var validator = serviceProvider.GetService(
+                typeof(IValidator<>).MakeGenericType(argument.GetType())) as IValidator;
+
+            if (validator is null)
+                continue;
+
+            var validationContext = new ValidationContext<object>(argument);
+            var result = await validator.ValidateAsync(validationContext, context.HttpContext.RequestAborted);
+
+            if (result.IsValid)
+                continue;
+
+            foreach (var group in result.Errors.GroupBy(e => e.PropertyName))
+                errors[group.Key] = group.Select(e => e.ErrorMessage).ToArray();
+        }
+
+        if (errors.Count > 0)
+        {
+            context.Result = new BadRequestObjectResult(new ValidationProblemDetails(errors)
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Title = "BadRequest",
+                Detail = "One or more validation errors occurred."
+            });
+            return;
+        }
+
+        await next();
+    }
+}
