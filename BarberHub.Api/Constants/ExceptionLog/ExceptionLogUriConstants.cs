5bfb028f94aa08aa4f6e3bc99c20f1afd2d438ee
abbas <abbasnader2025@gmail.com>
2026-08-22 21:52:08 +0330

    feat: add exception log URI constants

---
+++ BarberHub.Api/Constants/ExceptionLog/ExceptionLogUriConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Api.Constants.ExceptionLog;
+
+public static class ExceptionLogUriConstants
+{
+    private const string ControllerName = "exceptionlog";
+
+    public const string GetAll = $"{ControllerName}";
+    public const string GetRecent = $"{ControllerName}/recent";
+    public const string GetById = $"{ControllerName}/{{id}}";
+}
