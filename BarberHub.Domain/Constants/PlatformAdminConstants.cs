73a54ade955782ab95f2b2d359adbaf2a98d1cbc
abbas <abbasnader2025@gmail.com>
2026-08-24 17:08:43 +0330

    feat: add platform admin constants

---
+++ BarberHub.Domain/Constants/PlatformAdminConstants.cs
@@ -0,0 +1,9 @@
+namespace BarberHub.Domain.Constants;
+
+public static class PlatformAdminConstants
+{
+    public const int FirstNameMaxLength = 50;
+    public const int LastNameMaxLength = 50;
+    public const int UsernameMaxLength = 50;
+    public const int PasswordMaxLength = 255;
+}
