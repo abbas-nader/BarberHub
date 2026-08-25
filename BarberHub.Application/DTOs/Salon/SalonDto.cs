f4d379126d55213bb7ca9fc6cacda6a10619ef83
abbas <abbasnader2025@gmail.com>
2026-08-25 22:00:39 +0330

    feat: add salon dto

---
+++ BarberHub.Application/DTOs/Salon/SalonDto.cs
@@ -0,0 +1,15 @@
+﻿using BarberHub.Domain.Enums;
+using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Application.DTOs.Salon;
+
+public record SalonDto(
+    long Id,
+    string Name,
+    string Address,
+    string City,
+    string PhoneNumber,
+    decimal DepositAmountValue,
+    Currency DepositAmountCurrency,
+    string? Description,
+    bool IsActive);
