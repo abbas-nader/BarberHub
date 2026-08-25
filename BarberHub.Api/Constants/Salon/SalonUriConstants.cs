7e1304c6ce227609e508ec0081866abfceb9d433
abbas <abbasnader2025@gmail.com>
2026-08-25 23:45:27 +0330

    feat: add salon uri constants

---
+++ BarberHub.Api/Constants/Salon/SalonUriConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberHub.Api.Constants.Salon;
+
+public static class SalonUriConstants
+{
+    private const string ControllerName = "Barber";
+    public const string GetAll = $"{ControllerName}";
+    public const string GetById = $"{ControllerName}/{{id}}";
+    public const string Create = $"{ControllerName}/create";
+    public const string Update = $"{ControllerName}/update";
+    public const string Delete = $"{ControllerName}/delete";
+    public const string Activate = $"{ControllerName}/activate";
+    public const string Deactivate = $"{ControllerName}/deactivate";
+    public const string UpdateDepositAmount = $"{ControllerName}/updateDepositAmount";
+}
