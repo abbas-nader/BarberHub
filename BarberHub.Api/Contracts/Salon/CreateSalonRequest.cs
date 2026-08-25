747c63d1b96c34b5bb41c6fd9b6aeba2d2245be9
abbas <abbasnader2025@gmail.com>
2026-08-25 23:39:45 +0330

    feat: add create salon request

---
+++ BarberHub.Api/Contracts/Salon/CreateSalonRequest.cs
@@ -0,0 +1,13 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Api.Contracts.Salon;
+
+public record CreateSalonRequest(
+    string Name,
+    string Address,
+    string City,
+    string PhoneNumber,
+    decimal DepositAmountValue,
+    Currency DepositAmountCurrency,
+    string? Description
+);
