b6bf815dec89e695ece87e1f789500a1c3349a48
abbas <abbasnader2025@gmail.com>
2026-08-25 23:41:03 +0330

    feat: add update salon validation messages

---
+++ BarberHub.Api/Validators/Messages/Salon/UpdateSalonValidationMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberHub.Api.Validators.Messages.Salon;
+
+public class UpdateSalonValidationMessages
+{
+    public const string NameProperty = "Name";
+    public const string AddressProperty = "Address";
+    public const string CityProperty = "City";
+
+    public const string PhoneNumberProperty = "PhoneNumber";
+    public const string PhoneNumberInvalidFormat = "Phone number format is invalid.";
+    public const int PhoneNumberMaxLength = 11;
+
+    public const string DescriptionProperty = "Description";
+}
