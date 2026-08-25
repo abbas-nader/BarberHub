c09ffb1cf99d7d325cc6c7432bc703e7544ad106
abbas <abbasnader2025@gmail.com>
2026-08-25 23:40:05 +0330

    feat: add update salon request

---
+++ BarberHub.Api/Contracts/Salon/UpdateSalonRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Api.Contracts.Salon;
+
+public record UpdateSalonRequest(
+    long Id,
+    string Name,
+    string Address,
+    string City,
+    string PhoneNumber,
+    string? Description
+);
