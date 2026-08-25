88326e54f3c754b94f80a71fd4badbc64fc8f72f
abbas <abbasnader2025@gmail.com>
2026-08-25 23:38:32 +0330

    feat: add shared validation messages

---
+++ BarberHub.Api/Validators/Messages/Shared/SharedValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberHub.Api.Validators.Messages.Shared;
+
+public static class SharedValidationMessages
+{
+    public static string PropertyRequired(string propertyName) => $"{propertyName} is required.";
+
+    public static string PropertyMaxLength(string propertyName) =>
+        $"{propertyName} exceeds the maximum allowed length.";
+
+    public static string PropertyMinLength(string propertyName) =>
+        $"{propertyName} is shorter than the minimum required length.";
+}
