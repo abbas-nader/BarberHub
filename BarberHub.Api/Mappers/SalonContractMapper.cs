9f92e0b4e003116ad11f088669a3f79e192279ea
abbas <abbasnader2025@gmail.com>
2026-08-25 23:46:09 +0330

    feat: add salon contract mapper

---
+++ BarberHub.Api/Mappers/SalonContractMapper.cs
@@ -0,0 +1,42 @@
+﻿using BarberHub.Api.Contracts.Salon;
+using BarberHub.Application.DTOs.Salon;
+using BarberHub.Domain.ValueObjects;
+
+namespace BarberHub.Api.Mappers;
+
+public static class SalonContractMapper
+{
+    public static SalonResponse ToResponse(SalonDto salonDto)
+        => new(
+            salonDto.Id,
+            salonDto.Name,
+            salonDto.Address,
+            salonDto.City,
+            salonDto.PhoneNumber,
+            salonDto.DepositAmountValue,
+            salonDto.DepositAmountCurrency,
+            salonDto.Description,
+            salonDto.IsActive
+        );
+
+    public static CreateSalonDto ToDto(CreateSalonRequest request)
+        => new(
+            request.Name,
+            request.Address,
+            request.City,
+            request.PhoneNumber,
+            request.DepositAmountValue,
+            request.DepositAmountCurrency,
+            request.Description
+        );
+
+    public static UpdateSalonDto ToDto(UpdateSalonRequest request)
+        => new(
+            request.Id,
+            request.Name,
+            request.Address,
+            request.City,
+            request.PhoneNumber,
+            request.Description
+        );
+}
