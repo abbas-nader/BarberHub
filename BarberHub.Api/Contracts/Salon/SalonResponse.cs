258f50374d82626769109ec678deb0d50d48fa96
abbas <abbasnader2025@gmail.com>
2026-08-25 23:40:21 +0330

    feat: add salon response

---
+++ BarberHub.Api/Contracts/Salon/SalonResponse.cs
@@ -0,0 +1,15 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Api.Contracts.Salon;
+
+public record SalonResponse(
+    long Id,
+    string Name,
+    string Address,
+    string City,
+    string PhoneNumber,
+    decimal DepositAmountValue,
+    Currency DepositAmountCurrency,
+    string? Description,
+    bool IsActive
+);
