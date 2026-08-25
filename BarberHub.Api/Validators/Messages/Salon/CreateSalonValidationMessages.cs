f1be085c5196f8084eee2834fc017053910ae6ff
abbas <abbasnader2025@gmail.com>
2026-08-25 23:41:36 +0330

    feat: add create salon validation messages

---
+++ BarberHub.Api/Validators/Messages/Salon/CreateSalonValidationMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace BarberHub.Api.Validators.Messages.Salon;
+
+public static class CreateSalonValidationMessages
+{
+    public const string NameProperty = "Name";
+    public const string AddressProperty = "Address";
+    public const string CityProperty = "City";
+
+    public const string PhoneNumberProperty = "PhoneNumber";
+    public const string PhoneNumberInvalidFormat = "Phone number format is invalid.";
+
+    public const string DepositAmountValueInvalid = "Deposit amount must be greater than zero.";
+
+    public const string DepositAmountCurrencyInvalid = "Deposit amount currency is invalid.";
+
+    public const string DescriptionProperty = "Description";
+}
