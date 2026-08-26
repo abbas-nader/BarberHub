4b4c05441c710aa9d28a7d2de0dcae5e7143d126
abbas <abbasnader2025@gmail.com>
2026-08-27 00:42:27 +0330

    feat: add create file dto

---
+++ BarberHub.Application/DTOs/File/CreateFileDto.cs
@@ -0,0 +1,12 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Application.DTOs.File;
+
+public record CreateFileDto(
+    string FileName,
+    string OriginFileName,
+    string Url,
+    string ContentType,
+    long Size,
+    StorageProvider StorageProvider
+);
