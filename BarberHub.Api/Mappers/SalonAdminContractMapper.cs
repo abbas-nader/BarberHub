e30a949f1430233365645b54c2fabb66a82bc0c4
abbas <abbasnader2025@gmail.com>
2026-08-27 00:50:49 +0330

    feat: add salon admin contract mapper

---
+++ BarberHub.Api/Mappers/SalonAdminContractMapper.cs
@@ -0,0 +1,36 @@
+﻿using BarberHub.Api.Contracts.SalonAdmin;
+using BarberHub.Application.DTOs.SalonAdmin;
+
+namespace BarberHub.Api.Mappers;
+
+public static class SalonAdminContractMapper
+{
+    public static SalonAdminResponse ToResponse(this SalonAdminDto dto)
+        => new(
+            dto.Id,
+            dto.FirstName,
+            dto.LastName,
+            dto.MobileNumber
+        );
+
+    public static CreateSalonAdminDto ToDto(this CreateSalonAdminRequest request)
+        => new(
+            request.FirstName,
+            request.LastName,
+            request.Username,
+            request.Password,
+            request.MobileNumber,
+            request.SalonId
+        );
+
+    public static UpdateSalonAdminDto ToDto(this UpdateSalonAdminRequest request)
+        => new(
+            request.Id,
+            request.FirstName,
+            request.LastName,
+            request.Username,
+            request.Password,
+            request.MobileNumber
+        );
+
+}
