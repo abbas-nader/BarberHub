c881f05f3b889bd89cb4af44aa1bd227ad0b6ad0
abbas <abbasnader2025@gmail.com>
2026-08-27 00:46:16 +0330

    feat: add create salon admin dto

---
+++ BarberHub.Application/DTOs/SalonAdmin/CreateSalonAdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Application.DTOs.SalonAdmin;
+
+public record CreateSalonAdminDto(
+    string FirstName,
+    string LastName,
+    string Username,
+    string Password,
+    string MobileNumber,
+    long SalonId
+);
