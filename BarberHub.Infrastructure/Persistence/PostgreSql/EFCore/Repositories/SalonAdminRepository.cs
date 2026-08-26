fc4757107576bd17e299dc620c1fe898f5a7b51d
abbas <abbasnader2025@gmail.com>
2026-08-27 00:45:44 +0330

    feat: implement salon admin repository methods

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/SalonAdminRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/SalonAdminRepository.cs
@@ -1,9 +1,23 @@
 ﻿using BarberHub.Application.Repositories;
 using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 
 public class SalonAdminRepository(BarberHubDbContext context)
     : BaseRepository<SalonAdmin>(context), ISalonAdminRepository
 {
+    public async Task<bool> ExistsByUserNameAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        return await BarberHubDbContext.SalonAdmins.AnyAsync(x => x.UserName == userName, cancellationToken);
+    }
+
+    public async Task<IReadOnlyCollection<SalonAdmin>> GetAllBySalonIdAsync(long salonId,
+        CancellationToken cancellationToken = default)
+    {
+        return await BarberHubDbContext.SalonAdmins
+            .Where(x => x.SalonId == salonId && x.IsDeleted == false)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
 }
