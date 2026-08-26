ef7123d0188bf968e4f074a149f40b1d3afffbb4
abbas <abbasnader2025@gmail.com>
2026-08-27 00:43:15 +0330

    feat: add file dto

---
+++ BarberHub.Application/DTOs/File/FileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberHub.Application.DTOs.File;
+
+public record FileDto(
+    string FileName,
+    string OriginFileName,
+    string Url,
+    string ContentType,
+    long Size
+    );
