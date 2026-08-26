40f5bda4d046d27c95fd9541f56b50e27955b4f8
abbas <abbasnader2025@gmail.com>
2026-08-27 00:45:17 +0330

    feat: add salon admin repository methods

--- BarberHub.Application/Repositories/ISalonAdminRepository.cs
+++ BarberHub.Application/Repositories/ISalonAdminRepository.cs
@@ -4,4 +4,6 @@ namespace BarberHub.Application.Repositories;
 
 public interface ISalonAdminRepository : IRepository<SalonAdmin>
 {
+    Task<bool> ExistsByUserNameAsync(string userName, CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<SalonAdmin>> GetAllBySalonIdAsync(long salonId, CancellationToken cancellationToken = default);
 }
