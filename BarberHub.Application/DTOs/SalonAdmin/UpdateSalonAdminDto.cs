4441de79c33786d277356e97cff4fd97f9e08869
abbas <abbasnader2025@gmail.com>
2026-08-27 00:46:29 +0330

    feat: add update salon admin dto

---
+++ BarberHub.Application/DTOs/SalonAdmin/UpdateSalonAdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Application.DTOs.SalonAdmin;
+
+public record UpdateSalonAdminDto(
+    long Id,
+    string FirstName,
+    string LastName,
+    string Username,
+    string? Password,
+    string MobileNumber
+    );
