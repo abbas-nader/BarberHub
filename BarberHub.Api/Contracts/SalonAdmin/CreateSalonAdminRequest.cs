7ae9f20416bfa62ba286c14ca81bc042cdad3f84
abbas <abbasnader2025@gmail.com>
2026-08-27 00:48:38 +0330

    feat: add create salon admin request

---
+++ BarberHub.Api/Contracts/SalonAdmin/CreateSalonAdminRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Api.Contracts.SalonAdmin;
+
+public record CreateSalonAdminRequest(
+    string FirstName,
+    string LastName,
+    string Username,
+    string Password,
+    string MobileNumber,
+    long SalonId
+);
