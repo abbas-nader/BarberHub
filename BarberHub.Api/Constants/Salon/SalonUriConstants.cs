813a58d6217ca5c538710b195499f60e64092c84
abbas <abbasnader2025@gmail.com>
2026-08-27 14:37:27 +0330

    refactor: add salon id to resource routes

--- BarberHub.Api/Constants/Salon/SalonUriConstants.cs
+++ BarberHub.Api/Constants/Salon/SalonUriConstants.cs
@@ -4,11 +4,11 @@ public static class SalonUriConstants
 {
     private const string ControllerName = "Salon";
     public const string GetAll = $"{ControllerName}";
-    public const string GetById = $"{ControllerName}/{{id}}";
+    public const string GetById = $"{ControllerName}/{{salonId}}";
     public const string Create = $"{ControllerName}/create";
-    public const string Update = $"{ControllerName}/update";
-    public const string Delete = $"{ControllerName}/delete";
-    public const string Activate = $"{ControllerName}/activate";
-    public const string Deactivate = $"{ControllerName}/deactivate";
-    public const string UpdateDepositAmount = $"{ControllerName}/updateDepositAmount";
+    public const string Update = $"{ControllerName}/update/{{salonId}}";
+    public const string Delete = $"{ControllerName}/delete/{{salonId}}";
+    public const string Activate = $"{ControllerName}/activate/{{salonId}}";
+    public const string Deactivate = $"{ControllerName}/deactivate/{{salonId}}";
+    public const string UpdateDepositAmount = $"{ControllerName}/updateDepositAmount/{{salonId}}";
 }
