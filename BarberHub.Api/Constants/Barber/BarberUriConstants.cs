83b233669c8a4287c14b7b4b8bd35ee5c564bc79
abbas <abbasnader2025@gmail.com>
2026-08-27 14:29:30 +0330

    refactor: add barber id to resource routes

--- BarberHub.Api/Constants/Barber/BarberUriConstants.cs
+++ BarberHub.Api/Constants/Barber/BarberUriConstants.cs
@@ -4,10 +4,10 @@ public static class BarberUriConstants
 {
     private const string ControllerName = "Barber";
     public const string GetAllBySalonId = $"{ControllerName}";
-    public const string GetById = $"{ControllerName}/{{id}}";
+    public const string GetById = $"{ControllerName}/{{barberId}}";
     public const string Create = $"{ControllerName}/create";
-    public const string Update = $"{ControllerName}/update";
-    public const string Delete = $"{ControllerName}/delete";
-    public const string Activate = $"{ControllerName}/activate";
-    public const string Deactivate = $"{ControllerName}/deactivate";
+    public const string Update = $"{ControllerName}/update/{{barberId}}";
+    public const string Delete = $"{ControllerName}/delete/{{barberId}}";
+    public const string Activate = $"{ControllerName}/activate/{{barberId}}";
+    public const string Deactivate = $"{ControllerName}/deactivate/{{barberId}}";
 }
