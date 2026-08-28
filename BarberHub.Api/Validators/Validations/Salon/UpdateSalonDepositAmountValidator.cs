c2c2c3fb956f65a72047835eb489e65057508530
abbas <abbasnader2025@gmail.com>
2026-08-28 23:39:54 +0330

    feat: add salon deposit amount validator

---
+++ BarberHub.Api/Validators/Validations/Salon/UpdateSalonDepositAmountValidator.cs
@@ -0,0 +1,19 @@
+﻿using BarberHub.Api.Contracts.Salon;
+using BarberHub.Api.Validators.Messages.Salon;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.Salon;
+
+public class UpdateSalonDepositAmountValidator: AbstractValidator<UpdateSalonDepositAmountRequest>
+{
+    public UpdateSalonDepositAmountValidator()
+    {
+        RuleFor(x => x.DepositAmountValue)
+            .GreaterThan(UpdateSalonDepositAmountValidationMessages.ValueMinValidValue)
+            .WithMessage(UpdateSalonDepositAmountValidationMessages.DepositAmountValueInvalid);
+
+        RuleFor(x => x.DepositAmountCurrency)
+            .IsInEnum()
+            .WithMessage(UpdateSalonDepositAmountValidationMessages.DepositAmountCurrencyInvalid);
+    }
+}
