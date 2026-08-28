6f40640546a8068a2d3d2987d82785710a6c4475
abbas <abbasnader2025@gmail.com>
2026-08-28 23:40:03 +0330

    feat: add validation messages for salon deposit amount

---
+++ BarberHub.Api/Validators/Messages/Salon/UpdateSalonDepositAmountValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberHub.Api.Validators.Messages.Salon;
+
+public static class UpdateSalonDepositAmountValidationMessages
+{
+    public const string DepositAmountValueProperty = "DepositAmountValue";
+    public const string DepositAmountValueInvalid = "Deposit amount must be greater than zero.";
+    public const int ValueMinValidValue = 0;
+    
+    public const string DepositAmountCurrencyProperty = "DepositAmountCurrency";
+    public const string DepositAmountCurrencyInvalid = "Deposit amount currency is invalid.";
+    
+}
