785af889a91ec534998a723f9cf6334c2e404bbb
abbas <abbasnader2025@gmail.com>
2026-08-28 23:39:13 +0330

    feat: add request contract for updating salon deposit amount

---
+++ BarberHub.Api/Contracts/Salon/UpdateSalonDepositAmountRequest.cs
@@ -0,0 +1,8 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Api.Contracts.Salon;
+
+public record UpdateSalonDepositAmountRequest(
+    decimal DepositAmountValue,
+    Currency DepositAmountCurrency
+    );
