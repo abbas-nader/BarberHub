c97f764226399f524ba97599e9d4c91dd08a9e61
abbas <abbasnader2025@gmail.com>
2026-08-29 15:02:18 +0330

    feat: add token result dto

---
+++ BarberHub.Application/Security/JwtToken/TokenResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Application.Security.JwtToken;
+
+public record TokenResult(
+    string AccessToken,
+    DateTimeOffset AccessTokenExpiresAt,
+    string RefreshToken,
+    DateTimeOffset RefreshTokenExpiresAt
+    );
