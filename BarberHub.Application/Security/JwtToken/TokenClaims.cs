731eb6fa73d475dfbe86a264aefc38dd39820fd8
abbas <abbasnader2025@gmail.com>
2026-08-29 15:03:17 +0330

    feat: add token claims model

---
+++ BarberHub.Application/Security/JwtToken/TokenClaims.cs
@@ -0,0 +1,9 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Application.Security.JwtToken;
+
+public record TokenClaims(
+        long UserId,
+        UserRole UserRole,
+        long? SalonId
+        );
