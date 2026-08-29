d96c9c5f13c745f0013774855e9defd77bb7585b
abbas <abbasnader2025@gmail.com>
2026-08-29 15:05:04 +0330

    feat: add jwt settings

---
+++ BarberHub.Infrastructure/Security/JwtToken/JwtSetting.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberHub.Infrastructure.Security.JwtToken;
+
+public class JwtSetting
+{
+    public const string JwtSettingsSectionName = "Jwt";
+
+    public string SecretKey { get; set; } =
+        "BDxZfOsOV0QSRR/qcTxh5BY1uEcJ6G5lWzNSSlTsr0Yk0KN6vtxIBbZleml2g0YQ1DpfpWL/aVN4k+DglwBVHw==";
+
+    public string Issuer { get; set; } = "BarberHub";
+    public string Audience { get; set; } = "BarberHubClient";
+    public int AccessTokenExpirationMinutes { get; set; } = 15;
+    public int RefreshTokenExpirationMinutes { get; set; } = 7;
+}
