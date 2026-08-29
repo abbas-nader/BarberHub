d3d9a258b669c9e7a85866e0a5843a23187f6f5c
abbas <abbasnader2025@gmail.com>
2026-08-29 15:08:19 +0330

    feat: add refresh token repository interface

---
+++ BarberHub.Application/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using BarberHub.Domain.Entities;
+
+namespace BarberHub.Application.Repositories;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByTokenHashAsync(string tokenHash,  CancellationToken cancellationToken =  default);
+    Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken = default);
+    void Update(RefreshToken refreshToken);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
