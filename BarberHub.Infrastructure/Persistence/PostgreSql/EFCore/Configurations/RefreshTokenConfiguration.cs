ab12c02960a742908a7dd7b8e8cf7290423ca185
abbas <abbasnader2025@gmail.com>
2026-08-29 15:04:06 +0330

    feat: add refresh token entity configuration

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using BarberHub.Domain.Constants;
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x=>x.TokenHash)
+            .HasMaxLength(RefreshTokenConstants.TokenHashMaxLength)
+            .IsRequired();
+        builder.Property(x=> x.UserId)
+            .IsRequired();
+        builder.Property(x=>x.Role)
+            .IsRequired();
+        builder.Property(x=> x.CreatedAt)
+            .IsRequired();
+        builder.Property(x=> x.ExpiresAt)
+            .IsRequired();
+        builder.Property(x=>x.IsRevoked)
+            .IsRequired();
+        
+        builder.HasIndex(x=>x.TokenHash).IsUnique();
+        
+        builder.HasOne<RefreshToken>()
+            .WithOne()
+            .HasForeignKey<RefreshToken>(x=>x.ReplacedByTokenId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
