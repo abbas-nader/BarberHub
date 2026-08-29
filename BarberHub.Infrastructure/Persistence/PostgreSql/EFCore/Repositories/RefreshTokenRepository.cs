d7bc0c4c04520540fe0372ee012118f93ca522f8
abbas <abbasnader2025@gmail.com>
2026-08-29 15:03:24 +0330

    feat: add refresh token repository

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,22 @@
+﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class RefreshTokenRepository(BarberHubDbContext context) : IRefreshTokenRepository
+{
+    public async Task<RefreshToken?> GetByTokenHashAsync(string tokenHash,
+        CancellationToken cancellationToken = default)
+        => await context.RefreshTokens.FirstOrDefaultAsync(x => x.TokenHash == tokenHash && x.IsRevoked == false,
+            cancellationToken);
+
+    public async Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken = default)
+        => await context.RefreshTokens.AddAsync(refreshToken, cancellationToken);
+
+    public void Update(RefreshToken refreshToken)
+    => context.RefreshTokens.Update(refreshToken);
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    => context.SaveChangesAsync(cancellationToken);
+}
