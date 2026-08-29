1baca29a7b7d16656d96ac19017ef6b2f28a16d8
abbas <abbasnader2025@gmail.com>
2026-08-29 15:03:03 +0330

    feat: add token hasher using sha256

---
+++ BarberHub.Infrastructure/Security/JwtToken/TokenHasher.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using BarberHub.Application.Security.JwtToken;
+using BarberHub.Domain.Exceptions.SharedExceptions;
+
+namespace BarberHub.Infrastructure.Security.JwtToken;
+
+public class TokenHasher: ITokenHasher
+{
+    public string Hash(string token)
+    {
+        if(string.IsNullOrWhiteSpace(token))
+            throw new RequiredFieldException(nameof(token));
+        
+        var bytes = Encoding.UTF8.GetBytes(token);
+        var hashBytes = SHA256.HashData(bytes);
+        return Convert.ToHexString(hashBytes);
+    }
+}
