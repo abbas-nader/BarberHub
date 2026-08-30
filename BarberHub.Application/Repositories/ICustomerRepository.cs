a85a31347e16f585447965a5b028f6c8056841ca
abbas <abbasnader2025@gmail.com>
2026-08-30 23:10:15 +0330

    feat: add username lookup to ICustomerRepository

--- BarberHub.Application/Repositories/ICustomerRepository.cs
+++ BarberHub.Application/Repositories/ICustomerRepository.cs
@@ -4,4 +4,5 @@ namespace BarberHub.Application.Repositories;
 
 public interface ICustomerRepository : IRepository<Customer>
 {
+    Task<Customer?> GetByUserNameAsync(string userName, CancellationToken cancellationToken = default);
 }
