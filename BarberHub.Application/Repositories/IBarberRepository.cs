c1707c512e6c87aec47722801a2b49abb5f0c30a
abbas <abbasnader2025@gmail.com>
2026-08-30 23:09:46 +0330

    feat: add username lookup to IBarberRepository

--- BarberHub.Application/Repositories/IBarberRepository.cs
+++ BarberHub.Application/Repositories/IBarberRepository.cs
@@ -5,5 +5,6 @@ namespace BarberHub.Application.Repositories;
 public interface IBarberRepository : IRepository<Barber>
 {
     Task<bool> ExistsByUserNameAsync(string userName, CancellationToken cancellationToken = default);
+    Task<Barber?> GetByUserNameAsync(string userName, CancellationToken cancellationToken = default);
     Task<IReadOnlyCollection<Barber>> GetAllBySalonIdAsync(long salonId, CancellationToken cancellationToken = default);
 }
