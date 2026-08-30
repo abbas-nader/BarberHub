c3cd4e30d6180287ee7a131be7cb95616ba4ff73
abbas <abbasnader2025@gmail.com>
2026-08-30 23:13:28 +0330

    feat: add refresh token reuse detected exception

---
+++ BarberHub.Domain/Exceptions/RefreshTokenReuseDetectedException.cs
@@ -0,0 +1,6 @@
+﻿namespace BarberHub.Domain.Exceptions;
+
+public class RefreshTokenReuseDetectedException()
+    : Exception("Refresh token reuse detected. All related sessions have been revoked.")
+{
+}
