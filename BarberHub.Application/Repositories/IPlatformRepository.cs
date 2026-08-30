d694d33d3099e2435df29cbb60b327cab52ece5f
abbas <abbasnader2025@gmail.com>
2026-08-30 23:11:01 +0330

    feat: add username lookup to IPlatformRepository

--- BarberHub.Application/Repositories/IPlatformRepository.cs
+++ BarberHub.Application/Repositories/IPlatformRepository.cs
@@ -4,4 +4,5 @@ namespace BarberHub.Application.Repositories;
 
 public interface IPlatformRepository : IRepository<PlatformAdmin>
 {
+    Task<PlatformAdmin?> GetByUserNameAsync(string userName, CancellationToken cancellationToken = default);
 }
