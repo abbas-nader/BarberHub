446be41fbe6fce3c479e9e34318d8dad3ff1195d
abbas <abbasnader2025@gmail.com>
2026-08-30 23:08:27 +0330

    feat: add customer lookup by username

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/CustomerRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/CustomerRepository.cs
@@ -1,8 +1,12 @@
 ﻿using BarberHub.Application.Repositories;
 using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 
 public class CustomerRepository(BarberHubDbContext context) : BaseRepository<Customer>(context), ICustomerRepository
 {
+    public async Task<Customer?> GetByUserNameAsync(string userName, CancellationToken cancellationToken = default)
+        => await BarberHubDbContext.Customers.FirstOrDefaultAsync(x => x.UserName == userName && x.IsDeleted == false,
+            cancellationToken);
 }
