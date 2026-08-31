9f2750171a9446ac5ce7ae554b37ec0a7dca0d2b
abbas <abbasnader2025@gmail.com>
2026-08-31 13:41:56 +0330

    feat: add auth contract mapper

---
+++ BarberHub.Api/Mappers/AuthContractMapper.cs
@@ -0,0 +1,22 @@
+﻿using BarberHub.Api.Contracts.Auth;
+using BarberHub.Application.DTOs.Auth;
+using BarberHub.Application.Security.JwtToken;
+
+namespace BarberHub.Api.Mappers;
+
+public static class AuthContractMapper
+{
+    public static LoginDto ToDto(this LoginRequest request)
+        => new(
+            request.Username,
+            request.Password
+        );
+
+    public static TokenResponse ToResponse(this TokenResult tokenResult)
+        => new(
+            tokenResult.AccessToken,
+            tokenResult.AccessTokenExpiresAt,
+            tokenResult.RefreshToken,
+            tokenResult.RefreshTokenExpiresAt
+        );
+}
