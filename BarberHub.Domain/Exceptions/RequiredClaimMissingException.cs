feac3f8dc205f3f0592cec6fe17e70a0a879d6d5
abbas <abbasnader2025@gmail.com>
2026-08-31 13:43:16 +0330

    feat: add required claim missing exception

---
+++ BarberHub.Domain/Exceptions/RequiredClaimMissingException.cs
@@ -0,0 +1,6 @@
+﻿namespace BarberHub.Domain.Exceptions;
+
+public class RequiredClaimMissingException(string claimType)
+    : Exception($"Required claim '{claimType}' was not found on the current user.")
+{
+}
