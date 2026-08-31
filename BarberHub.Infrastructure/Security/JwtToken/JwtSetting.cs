25c146ef004cab682c8c104e9fdfd0b3479b99c4
abbas <abbasnader2025@gmail.com>
2026-08-31 23:33:42 +0330

    security: remove hardcoded JWT secret key

--- BarberHub.Infrastructure/Security/JwtToken/JwtSetting.cs
+++ BarberHub.Infrastructure/Security/JwtToken/JwtSetting.cs
@@ -4,9 +4,7 @@ public class JwtSetting
 {
     public const string JwtSettingsSectionName = "Jwt";
 
-    public string SecretKey { get; set; } =
-        "BDxZfOsOV0QSRR/qcTxh5BY1uEcJ6G5lWzNSSlTsr0Yk0KN6vtxIBbZleml2g0YQ1DpfpWL/aVN4k+DglwBVHw==";
-
+    public string SecretKey { get; set; } = string.Empty;
     public string Issuer { get; set; } = "BarberHub";
     public string Audience { get; set; } = "BarberHubClient";
     public int AccessTokenExpirationMinutes { get; set; } = 15;
