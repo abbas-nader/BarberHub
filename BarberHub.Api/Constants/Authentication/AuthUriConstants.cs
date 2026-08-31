3bfc34db9a2a5bb854d05c9fc4ff017a824b9b00
abbas <abbasnader2025@gmail.com>
2026-08-31 13:42:12 +0330

    feat: add authentication uri constants

---
+++ BarberHub.Api/Constants/Authentication/AuthUriConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberHub.Api.Constants.Authentication;
+
+public static class AuthUriConstants
+{
+    private const string ControllerName = "Auth";
+    public const string LoginSalonAdmin = $"{ControllerName}/login/salon-admin";
+    public const string LoginBarber = $"{ControllerName}/login/barber";
+    public const string LoginCustomer = $"{ControllerName}/login/customer";
+    public const string LoginPlatformAdmin = $"{ControllerName}/login/platform-admin";
+    public const string Refresh = $"{ControllerName}/refresh";
+    public const string Revoke = $"{ControllerName}/revoke";
+}
