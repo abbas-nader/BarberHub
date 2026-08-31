93703ee9129eed62717373111df51ac713607baf
abbas <abbasnader2025@gmail.com>
2026-08-31 23:34:56 +0330

    refactor: rename InvalidDepositAmountException to InvalidMoneyAmountException

--- BarberHub.Domain/Exceptions/InvalidDepositAmountException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BarberHub.Domain.Exceptions;
-
-public class InvalidDepositAmountException()
-    : Exception("Invalid amount of deposit amount. deposit amount must be greater than 0.")
-{
-}
---
+++ BarberHub.Domain/Exceptions/InvalidMoneyAmountException.cs
@@ -0,0 +1,5 @@
+﻿namespace BarberHub.Domain.Exceptions;
+
+public class InvalidMoneyAmountException() : Exception("Money value cannot be negative.")
+{
+}
