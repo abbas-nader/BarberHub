1d9c2800a66fd337591f4f323f1f1481f06d3ed6
abbas <abbasnader2025@gmail.com>
2026-09-02 23:50:28 +0330

    feat: implement get services by salon id

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/ServiceRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/ServiceRepository.cs
@@ -1,8 +1,15 @@
 ﻿using BarberHub.Application.Repositories;
 using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 
 public class ServiceRepository(BarberHubDbContext context) : BaseRepository<Service>(context), IServiceRepository
 {
+    public async Task<IReadOnlyCollection<Service>> GetAllBySalonIdAsync(long salonId,
+        CancellationToken cancellationToken = default)
+        => await BarberHubDbContext.Services
+            .Where(x => x.SalonId == salonId && x.IsDeleted == false)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
 }
