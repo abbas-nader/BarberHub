7f26348a8bbbe274d5f6828ea87ed78a5c2e5cc3
abbas <abbasnader2025@gmail.com>
2026-09-02 14:57:16 +0330

    feat: add authorization to base controller

--- BarberHub.Api/Controllers/BaseController.cs
+++ BarberHub.Api/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BarberHub.Api.Controllers;
 
 [ApiController]
 [Route("api/v{version:apiVersion}/")]
+[Authorize]
 public abstract class BaseController : ControllerBase
 {
 }
