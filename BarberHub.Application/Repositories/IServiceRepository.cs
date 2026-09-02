8ef23d758b03d9c0686dc0ac30158af5bbbb81d9
abbas <abbasnader2025@gmail.com>
2026-09-02 23:50:02 +0330

    feat: add get services by salon id method

--- BarberHub.Application/Repositories/IServiceRepository.cs
+++ BarberHub.Application/Repositories/IServiceRepository.cs
@@ -4,4 +4,5 @@ namespace BarberHub.Application.Repositories;
 
 public interface IServiceRepository : IRepository<Service>
 {
+    Task<IReadOnlyCollection<Service>> GetAllBySalonIdAsync(long salonId, CancellationToken cancellationToken = default);
 }
