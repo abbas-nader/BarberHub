4269a2ba5fa225f78a7022fdf34fde966d6c3781
abbas <abbasnader2025@gmail.com>
2026-09-03 19:02:59 +0330

    refactor: remove salon id from update routes

--- BarberHub.Api/Constants/Salon/SalonUriConstants.cs
+++ BarberHub.Api/Constants/Salon/SalonUriConstants.cs
@@ -6,9 +6,9 @@ public static class SalonUriConstants
     public const string GetAll = $"{ControllerName}";
     public const string GetById = $"{ControllerName}/{{salonId}}";
     public const string Create = $"{ControllerName}/create";
-    public const string Update = $"{ControllerName}/update/{{salonId}}";
+    public const string Update = $"{ControllerName}/update";
     public const string Delete = $"{ControllerName}/delete/{{salonId}}";
     public const string Activate = $"{ControllerName}/activate/{{salonId}}";
     public const string Deactivate = $"{ControllerName}/deactivate/{{salonId}}";
-    public const string UpdateDepositAmount = $"{ControllerName}/updateDepositAmount/{{salonId}}";
+    public const string UpdateDepositAmount = $"{ControllerName}/updateDepositAmount";
 }
