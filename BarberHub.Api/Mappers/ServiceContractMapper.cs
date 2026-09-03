fda992a21630713bd132f46d9d5355353d92ab7b
abbas <abbasnader2025@gmail.com>
2026-09-03 19:12:34 +0330

    feat: add service contract mapper

---
+++ BarberHub.Api/Mappers/ServiceContractMapper.cs
@@ -0,0 +1,30 @@
+﻿using BarberHub.Api.Contracts.Service;
+using BarberHub.Application.DTOs.Service;
+
+namespace BarberHub.Api.Mappers;
+
+public static class ServiceContractMapper
+{
+    public static ServiceResponse ToResponse(this ServiceDto serviceDto)
+        => new(
+            serviceDto.Id,
+            serviceDto.Name,
+            serviceDto.Description,
+            serviceDto.Duration
+        );
+
+    public static CreateServiceDto ToDto(this CreateServiceRequest request)
+        => new(
+            request.Name,
+            request.Description,
+            request.Duration
+        );
+
+    public static UpdateServiceDto ToDto(this UpdateServiceRequest request)
+        => new(
+            request.Id,
+            request.Name,
+            request.Description,
+            request.Duration
+        );
+}
