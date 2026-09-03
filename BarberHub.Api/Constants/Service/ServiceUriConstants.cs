1df8fbb1b0814f9f4bb5d931a0eb74e48c4b3974
abbas <abbasnader2025@gmail.com>
2026-09-03 19:12:52 +0330

    feat: add service uri constants

---
+++ BarberHub.Api/Constants/Service/ServiceUriConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Api.Constants.Service;
+
+public class ServiceUriConstants
+{
+    private const string ControllerName = "Service";
+    public const string GetAll = $"{ControllerName}";
+    public const string GetById = $"{ControllerName}/{{serviceId}}";
+    public const string Create = $"{ControllerName}/create";
+    public const string Update = $"{ControllerName}/update/{{serviceId}}";
+    public const string Delete = $"{ControllerName}/delete/{{serviceId}}";
+}
