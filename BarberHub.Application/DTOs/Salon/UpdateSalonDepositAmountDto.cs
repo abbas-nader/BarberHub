d902b0d2bf3efe98b9a8c416ec2720d2726defce
abbas <abbasnader2025@gmail.com>
2026-09-03 19:00:43 +0330

    feat: add salon id to update salon deposit amount dto

--- BarberHub.Application/DTOs/Salon/UpdateSalonDepositAmountDto.cs
+++ BarberHub.Application/DTOs/Salon/UpdateSalonDepositAmountDto.cs
@@ -4,5 +4,6 @@ namespace BarberHub.Application.DTOs.Salon;
 
 public record UpdateSalonDepositAmountDto(
     decimal DepositAmountValue,
-    Currency Currency
+    Currency Currency,
+    long SalonId
     );
