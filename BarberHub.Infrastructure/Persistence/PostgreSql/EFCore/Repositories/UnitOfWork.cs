50edd90e3ee81456209a4ba9a661ce9bcfc5bbe2
abbas <abbasnader2025@gmail.com>
2026-09-04 12:19:46 +0330

    feat: implement unit of work

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using BarberHub.Application.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
+
+public class UnitOfWork(BarberHubDbContext context) : IUnitOfWork
+{
+    private IDbContextTransaction? _transaction;
+
+    public async Task BeginTransaction(CancellationToken cancellationToken = default)
+    {
+        _transaction = await context.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+    public async Task CommitTransaction(CancellationToken cancellationToken = default)
+    {
+        if (_transaction is null) return;
+        try
+        {
+            await _transaction.CommitAsync(cancellationToken);
+        }
+        finally
+        {
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+    }
+
+    public async Task RollbackTransaction(CancellationToken cancellationToken = default)
+    {
+        if (_transaction is null) return;
+        try
+        {
+            await _transaction.RollbackAsync(cancellationToken);
+        }
+        finally
+        {
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+    }
+}
