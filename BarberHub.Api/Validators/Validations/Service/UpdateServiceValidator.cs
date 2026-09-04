5a8be9990500d77292a8429f9dcdab1baae68807
abbas <abbasnader2025@gmail.com>
2026-09-04 13:00:48 +0330

    feat: add update service validator

---
+++ BarberHub.Api/Validators/Validations/Service/UpdateServiceValidator.cs
@@ -0,0 +1,29 @@
+﻿using BarberHub.Api.Contracts.Barber;
+using BarberHub.Api.Contracts.Service;
+using BarberHub.Api.Validators.Messages.Barber;
+using BarberHub.Api.Validators.Messages.Service;
+using BarberHub.Api.Validators.Messages.Shared;
+using BarberHub.Domain.Constants;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.Service;
+
+public class UpdateServiceValidator : AbstractValidator<UpdateServiceRequest>
+{
+    public UpdateServiceValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage(SharedValidationMessages.PropertyRequired(UpdateServiceValidationMessages.NameProperty))
+            .MaximumLength(ServiceConstants.NameMaxLength)
+            .WithMessage(SharedValidationMessages.PropertyMaxLength(UpdateServiceValidationMessages.NameProperty));
+
+        RuleFor(x => x.Description)
+            .MaximumLength(ServiceConstants.DescriptionMaxLength)
+            .WithMessage(SharedValidationMessages.PropertyMaxLength(UpdateServiceValidationMessages.DescriptionProperty));
+
+        RuleFor(x => x.Duration)
+            .GreaterThan(TimeSpan.Zero)
+            .WithMessage(UpdateServiceValidationMessages.DurationInvalid);
+    }
+}
