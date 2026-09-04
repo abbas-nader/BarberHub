85c19aafad6fb9d1dbf8c9f8490338d7c4a06d01
abbas <abbasnader2025@gmail.com>
2026-09-04 13:00:26 +0330

    feat: add create service validator

---
+++ BarberHub.Api/Validators/Validations/Service/CreateServiceValidator.cs
@@ -0,0 +1,27 @@
+﻿using BarberHub.Api.Contracts.Service;
+using BarberHub.Api.Validators.Messages.Service;
+using BarberHub.Api.Validators.Messages.Shared;
+using BarberHub.Domain.Constants;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.Service;
+
+public class CreateServiceValidator : AbstractValidator<CreateServiceRequest>
+{
+    public CreateServiceValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage(SharedValidationMessages.PropertyRequired(CreateServiceValidationMessages.NameProperty))
+            .MaximumLength(ServiceConstants.NameMaxLength)
+            .WithMessage(SharedValidationMessages.PropertyMaxLength(CreateServiceValidationMessages.NameProperty));
+
+        RuleFor(x => x.Description)
+            .MaximumLength(ServiceConstants.DescriptionMaxLength)
+            .WithMessage(SharedValidationMessages.PropertyMaxLength(CreateServiceValidationMessages.DescriptionProperty));
+
+        RuleFor(x => x.Duration)
+            .GreaterThan(TimeSpan.Zero)
+            .WithMessage(CreateServiceValidationMessages.DurationInvalid);
+    }
+}
