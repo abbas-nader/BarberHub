b908fed0332a808c1283d6839a92a90046e0d1a4
abbas <abbasnader2025@gmail.com>
2026-09-04 13:00:35 +0330

    feat: add create service validation messages

---
+++ BarberHub.Api/Validators/Messages/Service/CreateServiceValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberHub.Api.Validators.Messages.Service;
+
+public static class CreateServiceValidationMessages
+{
+    public const string NameProperty = "Name";
+    
+    public const string DescriptionProperty = "Description";
+    
+    public const string DurationProperty = "Duration";
+
+    public const string DurationInvalid = "Duration must be greater than zero.";
+}
