5086727111c886ba85720303c028ea49a0990b12
abbas <abbasnader2025@gmail.com>
2026-09-05 10:37:42 +0330

    refactor: remove minimum duration constant from service constants

--- BarberHub.Domain/Constants/ServiceConstants.cs
+++ BarberHub.Domain/Constants/ServiceConstants.cs
@@ -3,7 +3,6 @@
 public static class ServiceConstants
 {
     public const int DescriptionMaxLength = 1000;
-    public const int DurationMinValue = 0;
     public const int NameMaxLength = 50;
     
 }
