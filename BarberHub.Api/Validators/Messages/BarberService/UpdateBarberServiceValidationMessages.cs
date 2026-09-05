0be53581e0a26db74a68c5b692705fc5eae06284
abbas <abbasnader2025@gmail.com>
2026-09-05 13:10:17 +0330

    feat: add update barber service validation messages

---
+++ BarberHub.Api/Validators/Messages/BarberService/UpdateBarberServiceValidationMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace BarberHub.Api.Validators.Messages.BarberService;
+
+public static class UpdateBarberServiceValidationMessages
+{
+    public const string PriceValueProperty = "PriceValue";
+    public const string PriceValueInvalid = "Price must be greater than zero.";
+
+    public const string CurrencyProperty = "PriceCurrency";
+    public const string CurrencyInvalid = "Currency is invalid.";
+
+    public const string DurationProperty = "Duration";
+    public const string DurationInvalid = "Duration must be greater than zero.";
+}
