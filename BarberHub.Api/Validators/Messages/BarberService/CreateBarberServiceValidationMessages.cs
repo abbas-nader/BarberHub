1c5cb60b45aabdd865eb79d32bcf33993c3e01a6
abbas <abbasnader2025@gmail.com>
2026-09-05 13:09:50 +0330

    feat: add create barber service validation messages

---
+++ BarberHub.Api/Validators/Messages/BarberService/CreateBarberServiceValidationMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace BarberHub.Api.Validators.Messages.BarberService;
+
+public static class CreateBarberServiceValidationMessages
+{
+    public const string BarberIdProperty = "BarberId";
+    public const string BarberIdInvalid = "Barber id must be a positive number.";
+
+    public const string ServiceIdProperty = "ServiceId";
+    public const string ServiceIdInvalid = "Service id must be a positive number.";
+
+    public const string PriceValueProperty = "PriceValue";
+    public const string PriceValueInvalid = "Price must be greater than zero.";
+
+    public const string CurrencyProperty = "PriceCurrency";
+    public const string CurrencyInvalid = "Currency is invalid.";
+
+    public const string DurationProperty = "Duration";
+    public const string DurationInvalid = "Duration must be greater than zero.";
+}
