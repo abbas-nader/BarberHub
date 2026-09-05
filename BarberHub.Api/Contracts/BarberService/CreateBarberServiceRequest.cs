b2cbf0bffa6aab783474da8d13d821270178c880
abbas <abbasnader2025@gmail.com>
2026-09-05 13:08:48 +0330

    feat: add create barber service request

---
+++ BarberHub.Api/Contracts/BarberService/CreateBarberServiceRequest.cs
@@ -0,0 +1,11 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Api.Contracts.BarberService;
+
+public record CreateBarberServiceRequest(
+    long BarberId,
+    long ServiceId,
+    decimal PriceValue,
+    Currency PriceCurrency,
+    TimeSpan Duration
+);
