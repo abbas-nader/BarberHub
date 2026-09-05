5c2a559c7ef361b303f4a09786f40275dbb9d49b
abbas <abbasnader2025@gmail.com>
2026-09-05 13:09:41 +0330

    feat: add create barber service validator

---
+++ BarberHub.Api/Validators/Validations/BarberService/CreateBarberServiceValidator.cs
@@ -0,0 +1,30 @@
+﻿using BarberHub.Api.Validators.Messages.BarberService;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.BarberService;
+
+public class CreateBarberServiceValidator : AbstractValidator<Domain.Entities.BarberService>
+{
+    public CreateBarberServiceValidator()
+    {
+        RuleFor(x => x.BarberId)
+            .GreaterThan(0)
+            .WithMessage(CreateBarberServiceValidationMessages.BarberIdInvalid);
+
+        RuleFor(x => x.ServiceId)
+            .GreaterThan(0)
+            .WithMessage(CreateBarberServiceValidationMessages.ServiceIdInvalid);
+
+        RuleFor(x => x.Price.Value)
+            .GreaterThan(0)
+            .WithMessage(CreateBarberServiceValidationMessages.PriceValueInvalid);
+
+        RuleFor(x => x.Price.Currency)
+            .IsInEnum()
+            .WithMessage(CreateBarberServiceValidationMessages.CurrencyInvalid);
+
+        RuleFor(x => x.Duration)
+            .GreaterThan(TimeSpan.Zero)
+            .WithMessage(CreateBarberServiceValidationMessages.DurationInvalid);
+    }
+}
