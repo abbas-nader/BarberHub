762592b4be83e8ab1b744e4b92e72498a4e71b64
abbas <abbasnader2025@gmail.com>
2026-09-05 13:10:38 +0330

    feat: add barber service contract mappings

---
+++ BarberHub.Api/Mappers/BarberServiceContractMapper.cs
@@ -0,0 +1,33 @@
+﻿using BarberHub.Api.Contracts.BarberService;
+using BarberHub.Application.DTOs.BarberService;
+
+namespace BarberHub.Api.Mappers;
+
+public static class BarberServiceContractMapper
+{
+    public static BarberServiceResponse ToResponse(this BarberServiceDto serviceDto)
+        => new(
+            serviceDto.Id,
+            serviceDto.BarberId,
+            serviceDto.ServiceId,
+            serviceDto.PriceValue,
+            serviceDto.PriceCurrency,
+            serviceDto.Duration
+        );
+
+    public static CreateBarberServiceDto ToDto(this CreateBarberServiceRequest serviceDto)
+        => new(
+            serviceDto.BarberId,
+            serviceDto.ServiceId,
+            serviceDto.PriceValue,
+            serviceDto.PriceCurrency,
+            serviceDto.Duration
+        );
+    
+    public static UpdateBarberServiceDto ToDto(this UpdateBarberServiceRequest serviceDto)
+        => new(
+            serviceDto.PriceValue,
+            serviceDto.PriceCurrency,
+            serviceDto.Duration
+        );
+}
