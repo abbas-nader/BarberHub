a871b8e6d5498579f6fa271bb87a3b95ba57f7e1
abbas <abbasnader2025@gmail.com>
2026-09-05 13:08:55 +0330

    feat: add barber service response

---
+++ BarberHub.Api/Contracts/BarberService/BarberServiceResponse.cs
@@ -0,0 +1,12 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Api.Contracts.BarberService;
+
+public record BarberServiceResponse(
+    long Id,
+    long BarberId,
+    long ServiceId,
+    decimal PriceValue,
+    Currency Currency,
+    TimeSpan Duration
+);
