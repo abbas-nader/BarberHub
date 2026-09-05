1554db9d9ff50deb7e2afa358987919136c7da9c
abbas <abbasnader2025@gmail.com>
2026-09-05 13:10:07 +0330

    feat: add update barber service validator

---
+++ BarberHub.Api/Validators/Validations/BarberService/UpdateBarberServiceValidator.cs
@@ -0,0 +1,22 @@
+﻿using BarberHub.Api.Validators.Messages.BarberService;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.BarberService;
+
+public class UpdateBarberServiceValidator :  AbstractValidator<Domain.Entities.BarberService>
+{
+    public UpdateBarberServiceValidator()
+    {
+        RuleFor(x => x.Price.Value)
+            .GreaterThan(0)
+            .WithMessage(UpdateBarberServiceValidationMessages.PriceValueInvalid);
+
+        RuleFor(x => x.Price.Currency)
+            .IsInEnum()
+            .WithMessage(UpdateBarberServiceValidationMessages.CurrencyInvalid);
+
+        RuleFor(x => x.Duration)
+            .GreaterThan(TimeSpan.Zero)
+            .WithMessage(UpdateBarberServiceValidationMessages.DurationInvalid);
+    }
+}
