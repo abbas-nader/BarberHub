463321ff86b9133d8b872ec1df3ff868284b3ac9
abbas <abbasnader2025@gmail.com>
2026-09-05 13:09:03 +0330

    feat: add update barber service request

---
+++ BarberHub.Api/Contracts/BarberService/UpdateBarberServiceRequest.cs
@@ -0,0 +1,9 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Api.Contracts.BarberService;
+
+public record UpdateBarberServiceRequest(
+    decimal PriceValue,
+    Currency PriceCurrency,
+    TimeSpan Duration
+);
