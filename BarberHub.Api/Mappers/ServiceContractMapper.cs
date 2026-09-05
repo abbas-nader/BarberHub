86fcfef0aaae8a7a8107fbf50355168d0d646880
abbas <abbasnader2025@gmail.com>
2026-09-05 10:38:00 +0330

    refactor: remove duration from service contract mappings

--- BarberHub.Api/Mappers/ServiceContractMapper.cs
+++ BarberHub.Api/Mappers/ServiceContractMapper.cs
@@ -9,21 +9,18 @@ public static class ServiceContractMapper
         => new(
             serviceDto.Id,
             serviceDto.Name,
-            serviceDto.Description,
-            serviceDto.Duration
+            serviceDto.Description
         );
 
     public static CreateServiceDto ToDto(this CreateServiceRequest request)
         => new(
             request.Name,
-            request.Description,
-            request.Duration
+            request.Description
         );
 
     public static UpdateServiceDto ToDto(this UpdateServiceRequest request)
         => new(
             request.Name,
-            request.Description,
-            request.Duration
+            request.Description
         );
 }
