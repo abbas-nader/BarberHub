ac2ec0823777879a0b85b5bfa209034d29bbbbda
abbas <abbasnader2025@gmail.com>
2026-09-05 10:41:34 +0330

    feat: add create barber service dto

---
+++ BarberHub.Application/DTOs/BarberService/CreateBarberServiceDto.cs
@@ -0,0 +1,11 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Application.DTOs.BarberService;
+
+public record CreateBarberServiceDto(
+    long BarberId,
+    long ServiceId,
+    decimal PriceValue,
+    Currency Currency,
+    TimeSpan Duration
+);
