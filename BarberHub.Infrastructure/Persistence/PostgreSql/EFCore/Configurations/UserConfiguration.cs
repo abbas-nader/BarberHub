aca80f8145dfe3ce215f2cc674d63d7aaea53c98
abbas <abbasnader2025@gmail.com>
2026-09-06 23:54:02 +0330

    refactor: rename customers table to users

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/UserConfiguration.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/UserConfiguration.cs
@@ -9,6 +9,7 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
+        builder.ToTable("Users");
         builder.HasKey(m => m.Id);
         
         builder.Property(x=> x. FirstName)
