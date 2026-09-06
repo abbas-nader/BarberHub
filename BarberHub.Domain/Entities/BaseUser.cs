c8833416b148dac1033b5226ebe5323fd0c3c6c4
abbas <abbasnader2025@gmail.com>
2026-09-06 23:28:23 +0330

    refactor: extract shared user properties and validation into BaseUser

---
+++ BarberHub.Domain/Entities/BaseUser.cs
@@ -0,0 +1,63 @@
+﻿using BarberHub.Domain.Exceptions.SharedExceptions;
+
+namespace BarberHub.Domain.Entities;
+
+public class BaseUser : BaseEntity
+{
+    public string FirstName { get; private set; } = null!;
+    public string LastName { get; private set; } = null!;
+    public string UserName { get; private set; } = null!;
+    public string PasswordHash { get; private set; } = null!;
+
+    protected BaseUser()
+    {
+    }
+
+    protected BaseUser(string firstName, string lastName, string userName, string passwordHash)
+    {
+        ValidateName(firstName, lastName);
+        ValidateUserName(userName);
+        ValidatePasswordHash(passwordHash);
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
+        PasswordHash = passwordHash;
+    }
+
+    protected void Update(string firstName, string lastName, string userName, long modifiedBy)
+    {
+        ValidateName(firstName, lastName);
+        ValidateUserName(userName);
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
+        Modified(modifiedBy);
+    }
+
+    protected void ChangePassword(string passwordHash, long modifiedBy)
+    {
+        ValidatePasswordHash(passwordHash);
+        PasswordHash = passwordHash;
+        Modified(modifiedBy);
+    }
+
+    private static void ValidateName(string firstName, string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new RequiredFieldException(nameof(firstName));
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new RequiredFieldException(nameof(lastName));
+    }
+
+    private static void ValidateUserName(string userName)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+            throw new RequiredFieldException(nameof(userName));
+    }
+
+    private static void ValidatePasswordHash(string passwordHash)
+    {
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new RequiredFieldException(nameof(passwordHash));
+    }
+}
