2d17d28ca404b6e1b56d70014c140a43246bf8e1
abbas <abbasnader2025@gmail.com>
2026-09-07 13:13:41 +0330

    feat: add change password mapper

---
+++ BarberHub.Api/Mappers/ChangePasswordMapper.cs
@@ -0,0 +1,13 @@
+﻿using BarberHub.Api.Contracts.Shared;
+using BarberHub.Application.DTOs.Shared;
+
+namespace BarberHub.Api.Mappers;
+
+public static class ChangePasswordMapper
+{
+    public static ChangePasswordDto ToDto(this ChangePasswordRequest request)
+        => new(
+            request.OldPassword,
+            request.NewPassword
+        );
+}
