39f1f775c6ffc379f3b062fa00a8b62ac445c481
abbas <abbasnader2025@gmail.com>
2026-09-07 22:44:55 +0330

    feat: add cancellation window expired exception

---
+++ BarberHub.Domain/Exceptions/CancellationWindowExpiredException.cs
@@ -0,0 +1,6 @@
+﻿namespace BarberHub.Domain.Exceptions;
+
+public class CancellationWindowExpiredException()
+    : Exception("Appointment cannot be cancelled less than 12 hours before its start time.")
+{
+}
