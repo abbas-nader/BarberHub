8a117b5a2ba380b66c1792a1ecc5e39241f99050
abbas <abbasnader2025@gmail.com>
2026-09-07 13:11:49 +0330

    feat: add salon admin change password endpoint

--- BarberHub.Api/Controllers/V1/SalonAdminController.cs
+++ BarberHub.Api/Controllers/V1/SalonAdminController.cs
@@ -2,6 +2,7 @@
 using BarberHub.Api.Constants.SalonAdmin;
 using BarberHub.Api.Contracts;
 using BarberHub.Api.Contracts.SalonAdmin;
+using BarberHub.Api.Contracts.Shared;
 using BarberHub.Api.Mappers;
 using BarberHub.Application.Services;
 using BarberHub.Domain.Enums;
@@ -11,10 +12,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace BarberHub.Api.Controllers.V1;
 
 [ApiVersion("1.0")]
-[Authorize(Roles = nameof(UserRole.PlatformAdmin))]
 public class SalonAdminController(SalonAdminService salonAdminService) : BaseController
 {
     [HttpGet(SalonAdminUriConstants.GetAllBySalonId)]
+    [Authorize(Roles = nameof(UserRole.PlatformAdmin))]
     public async Task<ApiResult<IReadOnlyList<SalonAdminResponse>>> GetAllAsync([FromRoute]long salonId,
         CancellationToken cancellationToken = default)
     {
@@ -23,6 +24,7 @@ public class SalonAdminController(SalonAdminService salonAdminService) : BaseCon
     }
 
     [HttpGet(SalonAdminUriConstants.GetById)]
+    [Authorize(Roles = nameof(UserRole.PlatformAdmin))]
     public async Task<ApiResult<SalonAdminResponse>> GetByIdAsync([FromRoute] long salonAdminId,
         CancellationToken cancellationToken = default)
     {
@@ -31,6 +33,7 @@ public class SalonAdminController(SalonAdminService salonAdminService) : BaseCon
     }
 
     [HttpPost(SalonAdminUriConstants.Create)]
+    [Authorize(Roles = nameof(UserRole.PlatformAdmin))]
     public async Task<ApiResult<SalonAdminResponse>> CreateAsync(
         [FromBody] CreateSalonAdminRequest createSalonAdminRequest,
         CancellationToken cancellationToken = default)
@@ -41,6 +44,7 @@ public class SalonAdminController(SalonAdminService salonAdminService) : BaseCon
     }
 
     [HttpPut(SalonAdminUriConstants.Update)]
+    [Authorize(Roles = nameof(UserRole.SalonAdmin))]
     public async Task<ApiResult<SalonAdminResponse>> UpdateAsync(
         [FromRoute] long salonAdminId,
         [FromBody] UpdateSalonAdminRequest updateSalonAdminRequest,
@@ -55,6 +59,14 @@ public class SalonAdminController(SalonAdminService salonAdminService) : BaseCon
     public async Task<ApiResult<SalonAdminResponse>> DeleteAsync([FromRoute] long salonAdminId, CancellationToken cancellationToken = default)
     {
         var salonAdmin = await salonAdminService.DeleteAsync(salonAdminId, cancellationToken);
+        return salonAdmin.ToResponse();
+    }
+    [HttpPatch(SalonAdminUriConstants.ChangePassword)]
+    [Authorize(Roles = nameof(UserRole.SalonAdmin))]
+    public async Task<ApiResult<SalonAdminResponse>> ChangePasswordAsync([FromBody] ChangePasswordRequest request,
+        CancellationToken cancellationToken)
+    {
+        var salonAdmin = await salonAdminService.ChangePasswordAsync(request.ToDto(), cancellationToken);
         return salonAdmin.ToResponse();
     }
 }
