c3c33b734bb1991c27c9e4f27f45fac5130ee035
abbas <abbasnader2025@gmail.com>
2026-09-07 10:43:48 +0330

    feat: add login validator and validation messages

---
+++ BarberHub.Api/Validators/Messages/Auth/LoginValidationMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Api.Validators.Messages.Auth;
+
+public static class LoginValidationMessages
+{
+    public const string UsernameProperty = "Username";
+    public const string PasswordProperty = "Password";
+    public const string RoleInvalid = "User role is invalid.";
+}
---
+++ BarberHub.Api/Validators/Validations/Auth/LoginValidator.cs
@@ -0,0 +1,24 @@
+﻿using BarberHub.Api.Contracts.Auth;
+using BarberHub.Api.Validators.Messages.Auth;
+using BarberHub.Api.Validators.Messages.Shared;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.Auth;
+
+public class LoginValidator : AbstractValidator<LoginRequest>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty()
+            .WithMessage(SharedValidationMessages.PropertyRequired(LoginValidationMessages.UsernameProperty));
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage(SharedValidationMessages.PropertyRequired(LoginValidationMessages.PasswordProperty));
+
+        RuleFor(x => x.Role)
+            .IsInEnum()
+            .WithMessage(LoginValidationMessages.RoleInvalid);
+    }
+}
