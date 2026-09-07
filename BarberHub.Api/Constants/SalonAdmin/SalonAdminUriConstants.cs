aeeb6111b2b6f307142a7241ddba60e8dc302072
abbas <abbasnader2025@gmail.com>
2026-09-07 13:09:32 +0330

    feat: add salon admin change password route

--- BarberHub.Api/Constants/SalonAdmin/SalonAdminUriConstants.cs
+++ BarberHub.Api/Constants/SalonAdmin/SalonAdminUriConstants.cs
@@ -8,4 +8,5 @@ public static class SalonAdminUriConstants
     public const string Create = $"{ControllerName}/create";
     public const string Update = $"{ControllerName}/update/{{salonAdminId}}";
     public const string Delete = $"{ControllerName}/delete/{{salonAdminId}}";
+    public const string ChangePassword = $"{ControllerName}/change-password";
 }
