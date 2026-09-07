b3c663bfc3b0eab11311d5cb06b98c9ff9589ee4
abbas <abbasnader2025@gmail.com>
2026-09-07 10:37:54 +0330

    refactor: consolidate authentication login routes

---
+++ BarberHub.Api/Constants/Auth/AuthUriConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberHub.Api.Constants.Auth;
+
+public static class AuthUriConstants
+{
+    private const string ControllerName = "auth";
+    public const string Login = $"{ControllerName}/login";
+    public const string Refresh = $"{ControllerName}/refresh";
+    public const string Revoke = $"{ControllerName}/revoke";
+}
