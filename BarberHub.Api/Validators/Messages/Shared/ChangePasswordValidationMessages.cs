9d6c40fbb1cbc64bbd11af57f1702a9d8bcfe948
abbas <abbasnader2025@gmail.com>
2026-09-07 13:13:58 +0330

    feat: add change password validator

---
+++ BarberHub.Api/Validators/Messages/Shared/ChangePasswordValidationMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Api.Validators.Messages.Shared;
+
+public static class ChangePasswordValidationMessages
+{
+    public const string OldPasswordProperty = "OldPassword";
+    public const string NewPasswordProperty = "NewPassword";
+    public const string NewPasswordSameAsOld = "New password must be different from the old password.";
+}
---
+++ BarberHub.Api/Validators/Validations/Shared/ChangePasswordValidator.cs
@@ -0,0 +1,29 @@
+﻿using BarberHub.Api.Contracts.Shared;
+using BarberHub.Api.Validators.Messages.Shared;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.Shared;
+
+public class ChangePasswordValidator : AbstractValidator<ChangePasswordRequest>
+{
+    public ChangePasswordValidator()
+    {
+        RuleFor(x => x.OldPassword)
+            .NotEmpty()
+            .WithMessage(
+                SharedValidationMessages.PropertyRequired(ChangePasswordValidationMessages.OldPasswordProperty));
+
+        RuleFor(x => x.NewPassword)
+            .NotEmpty()
+            .WithMessage(
+                SharedValidationMessages.PropertyRequired(ChangePasswordValidationMessages.NewPasswordProperty))
+            .MinimumLength(PasswordValidationMessages.MinLength)
+            .WithMessage(
+                SharedValidationMessages.PropertyMinLength(ChangePasswordValidationMessages.NewPasswordProperty))
+            .MaximumLength(PasswordValidationMessages.MaxLength)
+            .WithMessage(
+                SharedValidationMessages.PropertyMaxLength(ChangePasswordValidationMessages.NewPasswordProperty))
+            .NotEqual(x => x.OldPassword)
+            .WithMessage(ChangePasswordValidationMessages.NewPasswordSameAsOld);
+    }
+}
