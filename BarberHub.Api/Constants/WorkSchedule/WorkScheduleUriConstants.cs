12b9faae34211024f332d6f0ce188ecdb80df780
abbas <abbasnader2025@gmail.com>
2026-09-09 15:16:06 +0330

    feat: add work schedule endpoint routes

---
+++ BarberHub.Api/Constants/WorkSchedule/WorkScheduleUriConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberHub.Api.Constants.WorkSchedule;
+
+public static class WorkScheduleUriConstants
+{
+    private const string ControllerName = "workschedule";
+    public const string GetAllByBarberId = $"{ControllerName}/{{barberId}}";
+    public const string GetById = $"{ControllerName}/{{workscheduleId}}";
+    public const string Create = $"{ControllerName}/create";
+    public const string Update = $"{ControllerName}/update/{{workScheduleId}}";
+    public const string Delete = $"{ControllerName}/delete/{{workScheduleId}}";
+}
