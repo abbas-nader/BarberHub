b4f85b8d15f420a1aabc3b330ebe06d8782aeb5a
abbas <abbasnader2025@gmail.com>
2026-09-09 15:09:02 +0330

    feat: add paged result model

---
+++ BarberHub.Application/DTOs/Shared/PagedResult.cs
@@ -0,0 +1,18 @@
+﻿namespace BarberHub.Application.DTOs.Shared;
+
+public record PagedResult<T>
+{
+    public IReadOnlyCollection<T> Items { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+    public PagedResult(IReadOnlyCollection<T> items, int pageNumber, int pageSize, int totalCount)
+    {
+        Items = items;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+}
