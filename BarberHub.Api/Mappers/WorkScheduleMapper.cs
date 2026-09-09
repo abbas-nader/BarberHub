989f19914e89b931e3773bb704c47071798418b6
abbas <abbasnader2025@gmail.com>
2026-09-09 15:08:48 +0330

    feat: add work schedule mappers

---
+++ BarberHub.Api/Mappers/WorkScheduleMapper.cs
@@ -0,0 +1,32 @@
+﻿using BarberHub.Api.Contracts.WorkSchedule;
+using BarberHub.Application.DTOs.WorkSchedule;
+using BarberHub.Domain.Entities;
+
+namespace BarberHub.Api.Mappers;
+
+public static class WorkScheduleMapper
+{
+    public static WorkScheduleResponse ToResponse(this WorkScheduleDto workScheduleDto)
+        => new(
+            workScheduleDto.Id,
+            workScheduleDto.StartTime,
+            workScheduleDto.EndTime,
+            workScheduleDto.DayOfWeek,
+            workScheduleDto.BarberId
+        );
+
+    public static CreateWorkScheduleDto ToDto(this CreateWorkScheduleRequest request)
+        => new(
+            request.StartTime,
+            request.EndTime,
+            request.DayOfWeek,
+            request.BarberId
+        );
+
+    public static UpdateWorkScheduleDto ToDto(this UpdateWorkScheduleRequest request)
+        => new(
+            request.StartTime,
+            request.EndTime,
+            request.DayOfWeek
+        );
+}
