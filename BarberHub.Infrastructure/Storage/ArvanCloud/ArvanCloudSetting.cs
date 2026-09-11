92aef82d7bedde1f823db98175699d8c76e24ffd
abbas <abbasnader2025@gmail.com>
2026-09-11 11:48:44 +0330

    feat: add arvan cloud storage settings

---
+++ BarberHub.Infrastructure/Storage/ArvanCloud/ArvanCloudSetting.cs
@@ -0,0 +1,13 @@
+﻿namespace BarberHub.Infrastructure.Storage.ArvanCloud;
+
+public class ArvanCloudSetting
+{
+    public const string SectionName = "ArvanStorage";
+
+    public string ServiceUrl { get; set; } = "https://s3.ir-thr-at1.arvanstorage.ir";
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = "barberhub-storage";
+    public string PublicBaseUrl { get; set; } = "https://barberhub-files.s3.ir-thr-at1.arvanstorage.ir";
+    public int AvailabilityCheckTimeoutSeconds { get; set; } = 3;
+}
