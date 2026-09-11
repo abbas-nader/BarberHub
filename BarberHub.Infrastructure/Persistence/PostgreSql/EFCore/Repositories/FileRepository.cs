62e606369fe9a99049f84b787c0710e0eca2965d
abbas <abbasnader2025@gmail.com>
2026-09-11 11:44:03 +0330

    feat: add storage provider filter to file repository

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/FileRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/FileRepository.cs
@@ -1,8 +1,17 @@
 ﻿using BarberHub.Application.Repositories;
+using BarberHub.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
 using File = BarberHub.Domain.Entities.File;
 
 namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 
 public class FileRepository(BarberHubDbContext context) : BaseRepository<File>(context), IFileRepository
 {
+    public async Task<IReadOnlyList<File>> GetAllByStorageProviderAsync(StorageProvider storageProvider,
+        CancellationToken cancellationToken = default)
+    {
+        return await BarberHubDbContext.Files
+            .Where(x => x.StorageProvider == storageProvider && x.IsDeleted == false)
+            .ToListAsync(cancellationToken);
+    }
 }
