d5c39367fafd368294fc35f65fd1ee7c61404f39
abbas <abbasnader2025@gmail.com>
2026-09-11 12:40:12 +0330

    add file storage reconciliation settings

---
+++ BarberHub.Infrastructure/BackgroundJobs/FileStorageReconciliationSetting.cs
@@ -0,0 +1,7 @@
+﻿namespace BarberHub.Infrastructure.BackgroundJobs;
+
+public class FileStorageReconciliationSetting
+{
+    public const string SectionName = "FileStorageReconciliation";
+    public int IntervalDays { get; set; } = 3;
+}
