4bf58276a92dcbec3c597e842f5cbb427119c163
abbas <abbasnader2025@gmail.com>
2026-09-11 12:19:03 +0330

    add local storage configuration settings

---
+++ BarberHub.Infrastructure/Storage/Local/LocalStorageSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberHub.Infrastructure.Storage.Local;
+
+public class LocalStorageSetting
+{
+    public const string SectionName = "LocalStorage";
+
+    public string RootPath { get; set; } = "wwwroot/uploads";
+    public string PublicUrl { get; set; } = "/uploads";
+}
