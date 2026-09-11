a056736690dabc31557bdbee923cb1f7687b8547
abbas <abbasnader2025@gmail.com>
2026-09-11 13:33:11 +0330

    configure file storage services and background reconciliation job dependency injection

--- BarberHub.Infrastructure/DependencyInjection.cs
+++ BarberHub.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,22 @@
-﻿using BarberHub.Application.Repositories;
+﻿using Amazon.S3;
+using BarberHub.Application.Repositories;
 using BarberHub.Application.Security.Hash;
 using BarberHub.Application.Security.Jwt;
+using BarberHub.Application.Storage;
+using BarberHub.Infrastructure.BackgroundJobs;
 using BarberHub.Infrastructure.Persistence.Mongo;
 using BarberHub.Infrastructure.Persistence.Mongo.Repositories;
 using BarberHub.Infrastructure.Persistence.PostgreSql.EFCore;
 using BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Repositories;
 using BarberHub.Infrastructure.Security.Hash;
 using BarberHub.Infrastructure.Security.Jwt;
+using BarberHub.Infrastructure.Storage;
+using BarberHub.Infrastructure.Storage.ArvanCloud;
+using BarberHub.Infrastructure.Storage.Local;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace BarberHub.Infrastructure;
 
@@ -41,6 +48,7 @@ public static class DependencyInjection
 
         services.AddMongo(configuration);
         services.AddJwt(configuration);
+        services.AddArvanStorage(configuration);
     }
 
     private static void AddMongo(this IServiceCollection services, IConfiguration configuration)
@@ -56,5 +64,29 @@ public static class DependencyInjection
     private static void AddJwt(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<JwtSetting>(configuration.GetSection(JwtSetting.JwtSettingsSectionName));
+    }
+
+    private static void AddArvanStorage(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ArvanCloudSetting>(configuration.GetSection(ArvanCloudSetting.SectionName));
+        services.Configure<LocalStorageSetting>(configuration.GetSection(LocalStorageSetting.SectionName));
+        services.Configure<FileStorageReconciliationSetting>(
+            configuration.GetSection(FileStorageReconciliationSetting.SectionName));
+        services.AddSingleton<IAmazonS3>(sp =>
+        {
+            var arvanOption = sp.GetRequiredService<IOptions<ArvanCloudSetting>>().Value;
+            var config = new AmazonS3Config
+            {
+                ServiceURL = arvanOption.ServiceUrl,
+                ForcePathStyle = true,
+            };
+            return new AmazonS3Client(arvanOption.AccessKey, arvanOption.SecretKey, config);
+        });
+        services.AddScoped<IFileStorageService, ArvanCloudStorageService>();
+        services.AddScoped<IProviderFileStorage, LocalFileStorageService>();
+        services.AddScoped<IArvanAvailabilityChecker, ArvanAvailabilityChecker>();
+        services.AddScoped<IFileStorageService, ResilientFileStorageService>();
+
+        services.AddHostedService<FileStorageReconciliationJob>();
     }
 }
