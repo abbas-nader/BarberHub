65edd61534807697e7d3a7cb410749ce992528f3
abbas <abbasnader2025@gmail.com>
2026-09-11 11:38:21 +0330

    feat: add arvan availability checker

---
+++ BarberHub.Application/Storage/IArvanAvailabilityChecker.cs
@@ -0,0 +1,8 @@
+﻿using BarberHub.Domain.Enums;
+
+namespace BarberHub.Application.Storage;
+
+public interface IArvanAvailabilityChecker
+{
+    Task<bool> IsAvailableAsync(CancellationToken cancellationToken = default);
+}
