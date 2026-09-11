9a7a7a622d51be35baab959d46435839e2fefcc1
abbas <abbasnader2025@gmail.com>
2026-09-11 11:37:51 +0330

    feat: add storage provider filter to file repository

--- BarberHub.Application/Repositories/IFileRepository.cs
+++ BarberHub.Application/Repositories/IFileRepository.cs
@@ -1,7 +1,10 @@
-﻿using File = BarberHub.Domain.Entities.File;
+﻿using BarberHub.Domain.Enums;
+using File = BarberHub.Domain.Entities.File;
 
 namespace BarberHub.Application.Repositories;
 
 public interface IFileRepository : IRepository<File>
 {
+    Task<IReadOnlyList<File>> GetAllByStorageProviderAsync(StorageProvider storageProvider,
+        CancellationToken cancellationToken = default);
 }
