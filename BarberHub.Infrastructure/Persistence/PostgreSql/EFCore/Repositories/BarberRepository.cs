38f147e10d74add4018a3290b80d32ca264b3db6
abbas <abbasnader2025@gmail.com>
2026-09-12 10:47:18 +0330

    fix: include inactive barbers in salon barber list

--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BarberRepository.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Repositories/BarberRepository.cs
@@ -21,7 +21,7 @@ public class BarberRepository(BarberHubDbContext context) : BaseRepository<Barbe
         CancellationToken cancellationToken = default)
     {
         return await BarberHubDbContext.Barbers
-            .Where(x => x.SalonId == salonId && !x.IsDeleted && x.IsActive)
+            .Where(x => x.SalonId == salonId && !x.IsDeleted)
             .AsNoTracking()
             .ToListAsync(cancellationToken);
     }
