20733dc373a53e81cfa74932b6b2ccfa32b507af
abbas <abbasnader2025@gmail.com>
2026-09-12 10:51:53 +0330

    fix: set minimum review rating to 1

--- BarberHub.Domain/Constants/ReviewConstants.cs
+++ BarberHub.Domain/Constants/ReviewConstants.cs
@@ -2,7 +2,7 @@
 
 public static class ReviewConstants
 {
-    public const byte RateMinLength = 0;
+    public const byte RateMinLength = 1;
     public const byte RateMaxLength = 5;
     public const int CommentMaxLength = 500;
     public const int ReplyMaxLength = 500;
