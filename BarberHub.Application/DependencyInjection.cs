99af4ff8279966f17e0d59a45cd8cf8ce3a14c38
abbas <abbasnader2025@gmail.com>
2026-09-14 00:34:00 +0330

    feat: register gallery service in dependency injection

--- BarberHub.Application/DependencyInjection.cs
+++ BarberHub.Application/DependencyInjection.cs
@@ -16,5 +16,6 @@ public static class DependencyInjection
         services.AddScoped<BarberServiceCatalogService>();
         services.AddScoped<WorkScheduleService>();
         services.AddScoped<FileService>();
+        services.AddScoped<GalleryService>();
     }
 }
