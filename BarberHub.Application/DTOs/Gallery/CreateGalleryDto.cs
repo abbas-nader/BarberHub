ad962a4e3d9179c11a3129d70c4b279d65a1bb70
abbas <abbasnader2025@gmail.com>
2026-09-14 00:33:13 +0330

    refactor: integrate file upload and deletion into gallery service

--- BarberHub.Application/DTOs/Gallery/CreateGalleryDto.cs
+++ BarberHub.Application/DTOs/Gallery/CreateGalleryDto.cs
@@ -1,7 +1,10 @@
 namespace BarberHub.Application.DTOs.Gallery;
 
 public record CreateGalleryDto(
+    Stream FileStream,
+    string OriginFileName,
+    string ContentType,
+    long Size,
     string? Caption,
-    long? BarberId,
-    long FileId
+    long? BarberId
 );
--- BarberHub.Application/Services/GalleryService.cs
+++ BarberHub.Application/Services/GalleryService.cs
@@ -1,17 +1,18 @@
+using BarberHub.Application.DTOs.File;
 using BarberHub.Application.DTOs.Gallery;
 using BarberHub.Application.Repositories;
 using BarberHub.Application.Security.Jwt;
 using BarberHub.Domain.Entities;
 using BarberHub.Domain.Exceptions;
-using File = BarberHub.Domain.Entities.File;
 
 namespace BarberHub.Application.Services;
 
 public class GalleryService(
     IGalleryRepository galleryRepository,
-    IFileRepository fileRepository,
+    FileService fileService,
     IBarberRepository barberRepository,
-    ICurrentUserService currentUserService)
+    ICurrentUserService currentUserService,
+    IUnitOfWork unitOfWork)
 {
     public async Task<IReadOnlyList<GalleryDto>> GetAllBySalonIdAsync(long salonId,
         CancellationToken cancellationToken = default)
@@ -33,10 +34,6 @@ public class GalleryService(
         var salonId = currentUserService.CurrentUser.SalonId ??
                       throw new RequiredClaimMissingException(nameof(TokenClaims.SalonId));
 
-        var file = await fileRepository.GetByIdAsync(createGalleryDto.FileId, cancellationToken);
-
-        if (file is null)
-            throw new EntityNotFoundException(nameof(File), createGalleryDto.FileId);
         if (createGalleryDto.BarberId is not null)
         {
             var barber = await barberRepository.GetByIdAsync(createGalleryDto.BarberId.Value, cancellationToken);
@@ -44,15 +41,34 @@ public class GalleryService(
                 throw new EntityNotFoundException(nameof(Barber), createGalleryDto.BarberId.Value);
         }
 
-        var gallery = new Gallery(createGalleryDto.Caption, salonId, createGalleryDto.BarberId,
-            createGalleryDto.FileId, currentUserService.CurrentUser.UserId);
+        var uploadFileDto = new UploadFileDto(
+            createGalleryDto.FileStream,
+            createGalleryDto.OriginFileName,
+            createGalleryDto.ContentType,
+            createGalleryDto.Size
+        );
+        await unitOfWork.BeginTransaction(cancellationToken);
+        try
+        {
+            var uploadedFile = await fileService.UploadAsync(uploadFileDto, cancellationToken);
 
-        await galleryRepository.AddAsync(gallery, cancellationToken);
-        await galleryRepository.SaveChangesAsync(cancellationToken);
-        return ToDto(gallery);
+            var gallery = new Gallery(createGalleryDto.Caption, salonId, createGalleryDto.BarberId,
+                uploadedFile.Id, currentUserService.CurrentUser.UserId);
+
+            await galleryRepository.AddAsync(gallery, cancellationToken);
+            await galleryRepository.SaveChangesAsync(cancellationToken);
+
+            await unitOfWork.CommitTransaction(cancellationToken);
+            return ToDto(gallery);
+        }
+        catch
+        {
+            await unitOfWork.RollbackTransaction(cancellationToken);
+            throw;
+        }
     }
 
-    public async Task<GalleryDto> UpdateAsync(long galleryId, UpdateCaptionDto updateCaptionDto,
+    public async Task<GalleryDto> UpdateAsync(long galleryId, UpdateGalleryCaptionDto updateGalleryCaptionDto,
         CancellationToken cancellationToken = default)
     {
         var gallery = await galleryRepository.GetByIdAsync(galleryId, cancellationToken) ??
@@ -61,7 +77,7 @@ public class GalleryService(
                       throw new RequiredClaimMissingException(nameof(TokenClaims.SalonId));
         if (gallery.SalonId != salonId)
             throw new EntityNotFoundException(nameof(Gallery), galleryId);
-        gallery.UpdateCaption(updateCaptionDto.Caption, currentUserService.CurrentUser.UserId);
+        gallery.UpdateCaption(updateGalleryCaptionDto.Caption, currentUserService.CurrentUser.UserId);
         galleryRepository.Update(gallery);
         await galleryRepository.SaveChangesAsync(cancellationToken);
         return ToDto(gallery);
@@ -75,9 +91,22 @@ public class GalleryService(
                       throw new RequiredClaimMissingException(nameof(TokenClaims.SalonId));
         if (gallery.SalonId != salonId)
             throw new EntityNotFoundException(nameof(Gallery), galleryId);
-        gallery.SoftDelete(currentUserService.CurrentUser.UserId);
-        await galleryRepository.SaveChangesAsync(cancellationToken);
-        return ToDto(gallery);
+        await unitOfWork.BeginTransaction(cancellationToken);
+        try
+        {
+            gallery.SoftDelete(currentUserService.CurrentUser.UserId);
+            await galleryRepository.SaveChangesAsync(cancellationToken);
+
+            await fileService.DeleteAsync(gallery.FileId, cancellationToken);
+
+            await unitOfWork.CommitTransaction(cancellationToken);
+            return ToDto(gallery);
+        }
+        catch
+        {
+            await unitOfWork.RollbackTransaction(cancellationToken);
+            throw;
+        }
     }
 
     private static GalleryDto ToDto(Gallery gallery)
