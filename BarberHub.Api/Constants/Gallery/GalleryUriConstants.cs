801c4e0d320e43a819ceb7406da5f4e140e67957
abbas <abbasnader2025@gmail.com>
2026-09-14 00:36:47 +0330

    feat: add gallery controller endpoints

---
+++ BarberHub.Api/Constants/Gallery/GalleryUriConstants.cs
@@ -0,0 +1,12 @@
+namespace BarberHub.Api.Constants.Gallery;
+
+public static class GalleryUriConstants
+{
+    private const string ControllerName = "gallery";
+    
+    public const string GetAllBySalonId = $"{ControllerName}/salon/{{salonId}}";
+    public const string GetById = $"{ControllerName}/{{galleryId}}";
+    public const string Create = $"{ControllerName}/create";
+    public const string Update = $"{ControllerName}/update/{{galleryId}}";
+    public const string Delete = $"{ControllerName}/delete/{{galleryId}}";
+}
---
+++ BarberHub.Api/Controllers/V1/GalleryController.cs
@@ -0,0 +1,61 @@
+using Asp.Versioning;
+using BarberHub.Api.Constants.Gallery;
+using BarberHub.Api.Contracts;
+using BarberHub.Api.Contracts.Gallery;
+using BarberHub.Api.Mappers;
+using BarberHub.Application.Services;
+using BarberHub.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberHub.Api.Controllers.V1;
+
+[ApiVersion("1.0")]
+public class GalleryController(GalleryService galleryService) : BaseController
+{
+    [HttpGet(GalleryUriConstants.GetAllBySalonId)]
+    [AllowAnonymous]
+    public async Task<ApiResult<IReadOnlyList<GalleryResponse>>> GetAllBySalonId([FromRoute] long salonId,
+        CancellationToken cancellationToken = default)
+    {
+        var galleries = await galleryService.GetAllBySalonIdAsync(salonId, cancellationToken);
+        return galleries.Select(x => x.ToResponse()).ToList();
+    }
+
+    [HttpGet(GalleryUriConstants.GetById)]
+    [AllowAnonymous]
+    public async Task<ApiResult<GalleryResponse>> GetById([FromRoute] long salonId,
+        CancellationToken cancellationToken = default)
+    {
+        var galleries = await galleryService.GetByIdAsync(salonId, cancellationToken);
+        return galleries.ToResponse();
+    }
+
+    [HttpPost(GalleryUriConstants.Create)]
+    [Authorize(Roles = nameof(UserRole.SalonAdmin))]
+    public async Task<ApiResult<GalleryResponse>> Create([FromForm] CreateGalleryRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        var gallery = await galleryService.CreateAsync(request.ToDto(), cancellationToken);
+        return gallery.ToResponse();
+    }
+
+    [HttpPut(GalleryUriConstants.Update)]
+    [Authorize(Roles = nameof(UserRole.SalonAdmin))]
+    public async Task<ApiResult<GalleryResponse>> Update([FromRoute] long galleryId,
+        [FromBody] UpdateGalleyCaptionRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        var gallery = await galleryService.UpdateAsync(galleryId, request.ToDto(), cancellationToken);
+        return gallery.ToResponse();
+    }
+
+    [HttpPatch(GalleryUriConstants.Delete)]
+    [Authorize(Roles = nameof(UserRole.SalonAdmin))]
+    public async Task<ApiResult<GalleryResponse>> Delete([FromRoute] long galleryId,
+        CancellationToken cancellationToken = default)
+    {
+        var gallery = await galleryService.DeleteAsync(galleryId, cancellationToken);
+        return gallery.ToResponse();
+    }
+}
