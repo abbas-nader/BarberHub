544461d0c3beab98642637a46da75dd491200e10
abbas <abbasnader2025@gmail.com>
2026-09-13 18:03:46 +0330

    feat: add file URI constants

---
+++ BarberHub.Api/Constants/File/FileUriConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Api.Constants.File;
+
+public static class FileUriConstants
+{
+    private const string ControllerName = "file";
+
+    public const string Upload = $"{ControllerName}/upload";
+    public const string GetById = $"{ControllerName}/{{fileId}}";
+    public const string Delete = $"{ControllerName}/delete/{{fileId}}";
+}
