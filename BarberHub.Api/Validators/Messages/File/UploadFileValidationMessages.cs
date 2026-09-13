c7092d9156cf796a61af4c081a72ce830636ad86
abbas <abbasnader2025@gmail.com>
2026-09-13 18:04:08 +0330

    feat: add file upload validation messages and validator

---
+++ BarberHub.Api/Validators/Messages/File/UploadFileValidationMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace BarberHub.Api.Validators.Messages.File;
+
+public static class UploadFileValidationMessages
+{
+    public const string FileStreamProperty = "FileStream";
+
+    public const string OriginFileNameProperty = "OriginFileName";
+
+    public const string ContentTypeProperty = "ContentType";
+    public const string ContentTypeUnsupported = "File type is not supported.";
+
+    public const string SizeProperty = "Size";
+    public const string SizeInvalid = "File size must be greater than zero.";
+    public const string SizeExceeded = "File size exceeds the maximum allowed limit.";
+}
---
+++ BarberHub.Api/Validators/Validations/File/UploadFileValidator.cs
@@ -0,0 +1,36 @@
+﻿using BarberHub.Api.Contracts.File;
+using BarberHub.Api.Validators.Messages.File;
+using BarberHub.Api.Validators.Messages.Shared;
+using BarberHub.Application.Constants;
+using BarberHub.Domain.Constants;
+using FluentValidation;
+
+namespace BarberHub.Api.Validators.Validations.File;
+
+public class UploadFileValidator : AbstractValidator<UploadFileRequest>
+{
+    public UploadFileValidator()
+    {
+        RuleFor(x => x.FileStream)
+            .NotNull()
+            .WithMessage(SharedValidationMessages.PropertyRequired(UploadFileValidationMessages.FileStreamProperty));
+
+        RuleFor(x => x.OriginFileName)
+            .NotEmpty()
+            .WithMessage(SharedValidationMessages.PropertyRequired(UploadFileValidationMessages.OriginFileNameProperty))
+            .MaximumLength(FileConstants.OriginFileNameMaxLength)
+            .WithMessage(SharedValidationMessages.PropertyMaxLength(UploadFileValidationMessages.OriginFileNameProperty));
+
+        RuleFor(x => x.ContentType)
+            .NotEmpty()
+            .WithMessage(SharedValidationMessages.PropertyRequired(UploadFileValidationMessages.ContentTypeProperty))
+            .Must(contentType => FileUploadConstants.AllowedContentTypes.Contains(contentType))
+            .WithMessage(UploadFileValidationMessages.ContentTypeUnsupported);
+
+        RuleFor(x => x.Size)
+            .GreaterThan(FileConstants.SizeMinLength)
+            .WithMessage(UploadFileValidationMessages.SizeInvalid)
+            .LessThanOrEqualTo(FileUploadConstants.MaxSizeBytes)
+            .WithMessage(UploadFileValidationMessages.SizeExceeded);
+    }
+}
