9b8646c4c27c6d86ee1a15869ac040a8101697ce
abbas <abbasnader2025@gmail.com>
2026-09-13 18:04:23 +0330

    feat: add file contract mapper for request and response conversions

---
+++ BarberHub.Api/Mappers/FileContractMapper.cs
@@ -0,0 +1,27 @@
+﻿using BarberHub.Api.Constants.File;
+using BarberHub.Api.Contracts.File;
+using BarberHub.Application.DTOs.File;
+
+namespace BarberHub.Api.Mappers;
+
+public static class FileContractMapper
+{
+    public static FileResponse ToResponse(this FileDto fileDto)
+        => new
+        (
+            fileDto.Id,
+            fileDto.FileName,
+            fileDto.OriginFileName,
+            fileDto.Url,
+            fileDto.ContentType,
+            fileDto.Size
+        );
+
+    public static UploadFileDto ToDto(this UploadFileRequest request)
+        => new(
+            request.FileStream,
+            request.OriginFileName,
+            request.ContentType,
+            request.Size
+        );
+}
