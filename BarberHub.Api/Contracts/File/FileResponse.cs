bd5684c478eeeb078d8bb87294c2ac3362712baf
abbas <abbasnader2025@gmail.com>
2026-09-13 18:03:29 +0330

    feat: add file upload and response contracts

---
+++ BarberHub.Api/Contracts/File/FileResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberHub.Api.Contracts.File;
+
+public record FileResponse(
+    long Id,
+    string FileName,
+    string OriginFileName,
+    string Url,
+    string ContentType,
+    long Size
+);
---
+++ BarberHub.Api/Contracts/File/UploadFileRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberHub.Api.Contracts.File;
+
+public record UploadFileRequest(
+    Stream FileStream,
+    string OriginFileName,
+    string ContentType,
+    long Size
+);
