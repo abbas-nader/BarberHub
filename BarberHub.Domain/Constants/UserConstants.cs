6df1be2237dd6608d3946ce2787d53c847a1629b
abbas <abbasnader2025@gmail.com>
2026-09-14 21:17:55 +0330

    refactor: update user entity configuration and constants

--- BarberHub.Domain/Constants/UserConstants.cs
+++ BarberHub.Domain/Constants/UserConstants.cs
@@ -2,14 +2,9 @@
 
 public static class UserConstants
 {
-    public const string FirstNameColumnType = "varchar";
     public const int FirstNameMaxLength = 50;
-    public const string LastNameColumnType = "varchar";
     public const int LastNameMaxLength = 50;
-    public const string MobileNumberColumnType = "varchar";
     public const int MobileNumberMaxLength = 11;
-    public const string UsernameColumnType = "varchar";
     public const int UsernameMaxLength = 50;
-    public const string PasswordColumnType = "varchar";
     public const int PasswordMaxLength = 255;
 }
--- BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/UserConfiguration.cs
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/UserConfiguration.cs
@@ -7,34 +7,28 @@ namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
 
 public class UserConfiguration : IEntityTypeConfiguration<User>
 {
-    public void Configure(EntityTypeBuilder<User> builder)
-    {
-        builder.ToTable("Users");
-        builder.HasKey(m => m.Id);
-        
-        builder.Property(x=> x. FirstName)
-            .HasColumnType(UserConstants.FirstNameColumnType)
-            .HasMaxLength(UserConstants.FirstNameMaxLength)
-            .IsRequired();
-        builder.Property(x => x.LastName)
-            .HasColumnType(UserConstants.LastNameColumnType)
-            .HasMaxLength(UserConstants.LastNameMaxLength)
-            .IsRequired();
-        builder.Property(x=> x.MobileNumber)
-            .HasColumnType(UserConstants.MobileNumberColumnType)
-            .HasMaxLength(UserConstants.MobileNumberMaxLength)
-            .IsRequired();
-        builder.Property(x=> x.IsMobileVerified)
-            .IsRequired();
-        builder.Property(x=> x.UserName)
-            .HasColumnType(UserConstants.UsernameColumnType)
-            .HasMaxLength(UserConstants.UsernameMaxLength)
-            .IsRequired();
-        builder.Property(x=> x.PasswordHash)
-            .HasColumnType(UserConstants.PasswordColumnType)
-            .HasMaxLength(UserConstants.PasswordMaxLength)
-            .IsRequired();
-        
-        builder.HasIndex(x => x.UserName).IsUnique();
-    }
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(UserConstants.FirstNameMaxLength)
+                .IsRequired();
+            builder.Property(x => x.LastName)
+                .HasMaxLength(UserConstants.LastNameMaxLength)
+                .IsRequired();
+            builder.Property(x => x.UserName)
+                .HasMaxLength(UserConstants.UsernameMaxLength)
+                .IsRequired();
+            builder.Property(x => x.PasswordHash)
+                .HasMaxLength(UserConstants.PasswordMaxLength)
+                .IsRequired();
+            builder.Property(x => x.Role)
+                .IsRequired();
+            builder.Property(x => x.MobileNumber)
+                .HasMaxLength(UserConstants.MobileNumberMaxLength);
+
+            builder.HasIndex(x => x.UserName).IsUnique();
+        }
 }
