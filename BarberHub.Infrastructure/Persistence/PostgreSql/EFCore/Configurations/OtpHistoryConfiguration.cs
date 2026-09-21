d1cc39f7056cc77cfa48e2ed4a67c5a7a8ce55d5
abbas <abbasnader2025@gmail.com>
2026-09-21 23:41:36 +0330

    feat: configure OTP history entity persistence

---
+++ BarberHub.Infrastructure/Persistence/PostgreSql/EFCore/Configurations/OtpHistoryConfiguration.cs
@@ -0,0 +1,33 @@
+using BarberHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BarberHub.Infrastructure.Persistence.PostgreSql.EFCore.Configurations;
+
+public class OtpHistoryConfiguration : IEntityTypeConfiguration<OtpHistory>
+{
+    public void Configure(EntityTypeBuilder<OtpHistory> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Purpose)
+            .IsRequired();
+        builder.Property(x => x.UserId)
+            .IsRequired();
+        builder.Property(x => x.Status)
+            .IsRequired();
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+        builder.Property(x => x.ExpiresAt)
+            .IsRequired();
+        builder.Property(x => x.ResolvedAt)
+            .IsRequired(false);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => new { x.UserId, x.Purpose, x.CreatedAt });
+    }
+}
